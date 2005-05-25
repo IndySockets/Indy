401883755efa24eef0ee44618d8a1e4753c23bec
Ben Taylor <indy@svn.indy.org>
2005-05-25 14:46:42 +0000

    Fix delphi5 compile. Added IFDEF for proper finalization.

--- Indy10/Lib/Core/IdThread.pas
+++ Indy10/Lib/Core/IdThread.pas
@@ -430,7 +430,7 @@ begin
   // Therefore we raise the exceptions manually on D5 and below
   {$IFNDEF DOTNET}
   if (ThreadID = 0) then begin
-    RaiseLastOSError;
+    Sys.RaiseLastOSError;
   end;
   {$ENDIF}
   {$ENDIF}
@@ -601,5 +601,9 @@ finalization
   // This call hangs if not all threads have been properly destroyed.
   // But without this, bad threads can often have worse results. Catch 22.
 //  TIdThread.WaitAllThreadsTerminated;
-//  FreeAndNil(GThreadCount);
+
+  {$IFDEF IDFREEONFINAL}
+  //only enable this if you know your code exits thread-clean
+  Sys.FreeAndNil(GThreadCount);
+  {$ENDIF}
 end.
