0442594f1cca0f825f2a1d1a28e7d4ad6198d6ed
Ben Taylor <indy@svn.indy.org>
2005-05-27 08:19:40 +0000

    (empty log message)

---
+++ Indy10/Test/Core/IdTestSchedulerOfThreadPool.pas
@@ -0,0 +1,47 @@
+unit IdTestSchedulerOfThreadPool;
+
+interface
+
+uses
+  IdTest,
+  IdSchedulerOfThreadPool,
+  IdSys,
+  IdCmdTCPServer;
+
+type
+
+  TIdTestSchedulerOfThreadPool = class(TIdTest)
+  published
+    procedure btTestFree;
+  end;
+
+implementation
+
+procedure TIdTestSchedulerOfThreadPool.btTestFree;
+//tests that a simple start/stop doesn't cause errors
+var
+ //better to use a simple tcpserver?
+ aServer:TIdCmdTCPServer;
+ aSchedule:TIdSchedulerOfThreadPool;
+begin
+ aServer:=TIdCmdTCPServer.Create(nil);
+ aSchedule:=TIdSchedulerOfThreadPool.Create(nil);
+ try
+ aSchedule.PoolSize:=2;
+ aServer.Scheduler:=aSchedule;
+ //should set a port?
+ aServer.Active:=True;
+ //let things stabilize
+ //Sleep(200);
+ aServer.Active:=False;
+ finally
+ Sys.FreeAndNil(aServer);
+ Sys.FreeAndNil(aSchedule);
+ end;
+end;
+
+initialization
+
+  TIdTest.RegisterTest(TIdTestSchedulerOfThreadPool);
+
+end.
