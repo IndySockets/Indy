e3b2ca5d06fb304f1e1af7304044293b69ea148f
Ben Taylor <indy@svn.indy.org>
2005-05-27 07:30:53 +0000

    (empty log message)

---
+++ Indy10/Test/Core/IdTestSocketHandle.pas
@@ -0,0 +1,46 @@
+unit IdTestSocketHandle;
+
+//exposes a bug
+
+//function TIdStackWindows.WSGetServByPort(
+//bug returns invalid result
+
+interface
+
+uses
+  IdSocketHandle,
+  IdStack,
+  IdTest,
+  IdSys;
+
+type
+
+  TIdTestSocketHandle = class(TIdTest)
+  published
+    procedure TestSocketFree;
+  end;
+
+implementation
+
+procedure TIdTestSocketHandle.TestSocketFree;
+//used to be a bug with incorrect freeing critical section, then using it.
+var
+ s:TIdSocketHandle;
+begin
+
+ TIdStack.IncUsage;
+ s:=TIdSocketHandle.Create(nil);
+ try
+  s.AllocateSocket;
+ finally
+ Sys.FreeAndNil(s);
+ TIdStack.DecUsage;
+ end;
+
+end;
+
+initialization
+
+  TIdTest.RegisterTest(TIdTestSocketHandle);
+
+end.
