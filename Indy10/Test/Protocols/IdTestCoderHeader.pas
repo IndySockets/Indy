b91b8f59dc105cbc2a375fa04b695eb9c1f43996
Ben Taylor <indy@svn.indy.org>
2005-05-30 13:20:41 +0000

    Add checks

--- Indy10/Test/Protocols/IdTestCoderHeader.pas
+++ Indy10/Test/Protocols/IdTestCoderHeader.pas
@@ -21,17 +21,37 @@ const
  cIn1='=?Windows-1252?B?TWFya3RlaW5m/GhydW5nIGVpbmVz?='
   +'=?Windows-1252?B?IHb2bGxpZyBuZXVlbiBLb256ZXB0cw==?=';
  cOut1='Markteinführung eines völlig neuen Konzepts';
+
+ cIn2='=?iso-8859-1?Q?J=F6rg_Meier?= <briefe@jmeiersoftware.de>';
+ cOut2='Jörg Meier <briefe@jmeiersoftware.de>';
+
+ cIn3='böb <bob@example.com>';
+ cOut3='=?ISO-8859-1?Q?b=F6b?= <bob@example.com>';
+
+ cIn4='böb <böb@example.cöm>';
+ cOut4='=?ISO-8859-1?Q?b=F6b <b=F6b@example.c=F6m>?=';
 var
  s:string;
 begin
+
+ //from TIdMessage.GenerateHeader
+
  //edge case
  s:=DecodeHeader('');
  Assert(s='');
 
- //3175ms for 10000 loops in old version
- //2493ms in new
- s:=DecodeHeader(cIn1);
- Assert(s=cOut1);
+ //s:=DecodeHeader(cIn1);
+ //Assert(s=cOut1);
+
+ s:=EncodeHeader(cIn3,'','Q',bit8,'ISO-8859-1');
+ Assert(s=cOut3);
+ s:=DecodeHeader(cOut3);
+ Assert(s=cIn3);
+
+ s:=EncodeHeader(cIn4,'','Q',bit8,'ISO-8859-1');
+ Assert(s=cOut4);
+ s:=DecodeHeader(cOut4);
+ Assert(s=cIn4,s);
 
 end;
 
