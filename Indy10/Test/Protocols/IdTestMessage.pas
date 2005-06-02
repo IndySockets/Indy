e258f670a1f6e24aad0e8bb1a730d467654fe364
Ben Taylor <indy@svn.indy.org>
2005-06-02 13:20:02 +0000

    (empty log message)

---
+++ Indy10/Test/Protocols/IdTestMessage.pas
@@ -0,0 +1,42 @@
+unit IdTestMessage;
+
+interface
+
+uses
+  IdTest,
+  IdSys,
+  IdMessage;
+
+type
+
+  TIdTestMessage = class(TIdTest)
+  published
+    procedure TestAddresses;
+  end;
+
+implementation
+
+procedure TIdTestMessage.TestAddresses;
+//check that an address string is parsed correctly
+var
+ m:TIdMessage;
+const
+ cAddress1='abc@example.com';
+ cAddress2='xyz@example.com';
+begin
+ m:=TIdMessage.Create(nil);
+ try
+ m.ReplyTo.EMailAddresses:=cAddress1+';'+cAddress2;
+ Assert(m.ReplyTo.Count=2);
+ Assert(m.ReplyTo.Items[0].Address=cAddress1);
+ Assert(m.ReplyTo.Items[1].Address=cAddress2);
+ finally
+ Sys.FreeAndNil(m);
+ end;
+end;
+
+initialization
+
+  TIdTest.RegisterTest(TIdTestMessage);
+
+end.
