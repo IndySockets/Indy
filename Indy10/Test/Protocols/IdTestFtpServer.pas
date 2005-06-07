d8c30b70acbd968135828c01441aa251dd3a0421
Ben Taylor <indy@svn.indy.org>
2005-06-07 10:42:19 +0000

    (empty log message)

---
+++ Indy10/Test/Protocols/IdTestFtpServer.pas
@@ -0,0 +1,72 @@
+unit IdTestFtpServer;
+
+interface
+
+uses
+  IdSys,
+  IdTest,
+  IdTcpClient,
+  IdFtp,
+  IdFtpServer;
+
+type
+
+  TIdTestFtpServer = class(TIdTest)
+  published
+    procedure TestBasic;
+  end;
+
+implementation
+
+procedure TIdTestFtpServer.TestBasic;
+var
+ s:TIdFTPServer;
+ c:TIdTCPClient;
+ aStr:string;
+const
+ cGreeting='HELLO';
+ cTestFtpPort=20021;
+begin
+ s:=TIdFTPServer.Create(nil);
+ c:=TIdTCPClient.Create(nil);
+ try
+ s.Greeting.Text.Text:=cGreeting;
+ s.DefaultPort:=cTestFtpPort;
+ s.Active:=True;
+
+ c.Port:=cTestFtpPort;
+ c.Host:='127.0.0.1';
+ c.Connect;
+ c.IOHandler.ReadTimeout:=500;
+
+ //expect a greeting. typical="220 FTP Server Ready."
+ aStr:=c.IOHandler.Readln;
+ Assert(aStr='220 '+cGreeting);
+
+ //ftp server should only process a command after crlf
+ c.IOHandler.Write('U');
+ aStr:=c.IOHandler.Readln;
+ Assert(aStr='',aStr);
+
+ //complete the rest of the command
+ c.IOHandler.WriteLn('SER ANONYMOUS');
+ aStr:=c.IOHandler.Readln;
+ Assert(aStr<>'',aStr);
+
+ //attempt to start a transfer when no datachannel setup.
+ //should give 550 error?
+
+ //typical quit='221 Goodbye.'
+
+ finally
+ Sys.FreeAndNil(c);
+ Sys.FreeAndNil(s);
+ end;
+
+end;
+
+initialization
+
+  TIdTest.RegisterTest(TIdTestFtpServer);
+
+end.
