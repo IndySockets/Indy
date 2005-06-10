58f6a10cfd1349ba3ad06d94c9855c647d017ebb
Ben Taylor <indy@svn.indy.org>
2005-06-10 14:30:47 +0000

    Added stream tests

--- Indy10/Test/Core/IdTestBuffer.pas
+++ Indy10/Test/Core/IdTestBuffer.pas
@@ -12,12 +12,16 @@ type
 
   TIdTestBuffer = class(TIdTest)
   published
-    procedure TestStream;
+    procedure TestBufferToStream;
+    procedure TestStreamToBuffer;
   end;
 
 implementation
 
-procedure TIdTestBuffer.TestStream;
+const
+ cString='12345';
+
+procedure TIdTestBuffer.TestBufferToStream;
 var
  aBuffer:TIdBuffer;
  aStream:TIdStringStream;
@@ -25,8 +29,65 @@ begin
  aBuffer:=TIdBuffer.Create;
  aStream:=TIdStringStream.Create('');
  try
- aBuffer.Write('12345');
+ aBuffer.Write(cString);
+
  aBuffer.SaveToStream(aStream);
+ Assert(aStream.DataString=cString);
+ Assert(aBuffer.Size=5);
+
+ //extract with index=-1 and >0 to test execution paths
+
+ //index>-1 doesn't delete data from buffer
+ aStream.Size:=0;
+ aBuffer.ExtractToStream(aStream,2,3);
+ Assert(aStream.DataString='45');
+ Assert(aBuffer.Size=5);
+
+ //does delete data from buffer
+ aStream.Size:=0;
+ aBuffer.ExtractToStream(aStream,3);
+ Assert(aStream.DataString='123');
+ Assert(aBuffer.Size=2);
+
+ aStream.Size:=0;
+ aBuffer.ExtractToStream(aStream,2);
+ Assert(aStream.DataString='45');
+ Assert(aBuffer.Size=0);
+
+ finally
+ Sys.FreeAndNil(aStream);
+ Sys.FreeAndNil(aBuffer);
+ end;
+end;
+
+procedure TIdTestBuffer.TestStreamToBuffer;
+var
+ aBuffer:TIdBuffer;
+ aStream:TIdStringStream;
+begin
+ aBuffer:=TIdBuffer.Create;
+ aStream:=TIdStringStream.Create('');
+ try
+ aStream.WriteString(cString);
+ aStream.Position:=0;
+
+ //copy specific
+ aBuffer.Write(aStream,2);
+ Assert(aStream.Position=2);
+ Assert(aBuffer.AsString='12');
+
+ //copy remaining
+ aBuffer.Write(aStream,-1);
+ Assert(aStream.Position=5);
+ Assert(aBuffer.AsString=cString);
+
+ //copy all. ensure stream position is >0,<size
+ aBuffer.Clear;
+ aStream.Position:=2;
+ aBuffer.Write(aStream);
+ Assert(aStream.Position=5);
+ Assert(aBuffer.AsString=cString);
+
  finally
  Sys.FreeAndNil(aStream);
  Sys.FreeAndNil(aBuffer);
