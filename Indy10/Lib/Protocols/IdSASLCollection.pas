537947a4e521fb088befdba9d24fe6a13bf4fdcc
Ben Taylor <indy@svn.indy.org>
2005-06-13 13:35:29 +0000

    Updated File Header

--- Indy10/Lib/Protocols/IdSASLCollection.pas
+++ Indy10/Lib/Protocols/IdSASLCollection.pas
@@ -1,13 +1,20 @@
-{ $HDR$}
-{**********************************************************************}
-{ Unit archived using Team Coherence                                   }
-{ Team Coherence is Copyright 2002 by Quality Software Components      }
-{                                                                      }
-{ For further information / comments, visit our WEB site at            }
-{ http://www.TeamCoherence.com                                         }
-{**********************************************************************}
-{}
-{ $Log:  55713: IdSASLCollection.pas
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
 {
 {   Rev 1.5    11/27/2004 8:27:14 PM  JPMugaas
 { Fix for compiler errors.
--- Indy10/Lib/Protocols/IdServerIOHandlerSSLOpenSSL.pas
+++ Indy10/Lib/Protocols/IdServerIOHandlerSSLOpenSSL.pas
@@ -1,13 +1,20 @@
-{ $HDR$}
-{**********************************************************************}
-{ Unit archived using Team Coherence                                   }
-{ Team Coherence is Copyright 2002 by Quality Software Components      }
-{                                                                      }
-{ For further information / comments, visit our WEB site at            }
-{ http://www.TeamCoherence.com                                         }
-{**********************************************************************}
-{}
-{ $Log:  11745: IdServerIOHandlerSSLOpenSSL.pas 
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
 {
 {   Rev 1.1    2/6/2004 3:37:06 PM  JPMugaas
 { Fixed a refernce that was outdated.
