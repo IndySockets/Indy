7274ac2ca255a8990be51d833f2f023654f43e2a
Ben Taylor <indy@svn.indy.org>
2005-06-13 13:30:26 +0000

    Updated File Header

--- Indy10/Lib/Protocols/IdFTPListOutput.pas
+++ Indy10/Lib/Protocols/IdFTPListOutput.pas
@@ -1,13 +1,20 @@
-{ $HDR$}
-{**********************************************************************}
-{ Unit archived using Team Coherence                                   }
-{ Team Coherence is Copyright 2002 by Quality Software Components      }
-{                                                                      }
-{ For further information / comments, visit our WEB site at            }
-{ http://www.TeamCoherence.com                                         }
-{**********************************************************************}
-{}
-{ $Log:  16230: IdFTPListOutput.pas
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
 {
 {   Rev 1.18    12/10/04 1:13:34 PM  RLebeau
 { FormatDateTime() fixes.  Was using 'mm' instead of 'nn' for minutes.
