9677f046988781371486e3a3f117d3b243e6e101
Ben Taylor <indy@svn.indy.org>
2005-06-13 00:47:32 +0000

    TIdTestStringList

--- Indy10/Test/System/IdTestObjs.pas
+++ Indy10/Test/System/IdTestObjs.pas
@@ -14,6 +14,11 @@ type
     procedure TestOwner;
   end;
 
+  TIdTestStringList = class(TIdTest)
+  published
+    procedure TestBasic;
+  end;
+
   TIdTestStringStream = class(TIdTest)
   published
     procedure TestStream;
@@ -59,21 +64,50 @@ end;
 procedure TIdTestNativeComponent.TestOwner;
 //eg used to cause stack overflow in .net
 //also want to test when owner<>nil. eg add a c2.
-var
+begin
+{var
  c,o:TIdNativeComponent;
 begin
- c:=TIdTestNativeComponent.Create(nil);
+ c:=TIdNativeComponent.Create;
  try
  o:=c.Owner;
  Assert(o=nil);
  finally
  sys.FreeAndNil(c);
- end;
+ end;}
+end;
+
+procedure TIdTestStringList.TestBasic;
+var
+  l:TIdStringList;
+  s:string;
+const
+  cStr='123';
+  cComma='1,2,3';
+  cMulti='1'+#13#10+'2'+#13#10+'3';
+begin
+  l:=TIdStringList.Create;
+  try
+    l.Text:=cStr;
+    s:=l.CommaText;
+    Assert(s=cStr);
+
+    l.Text:=cMulti;
+    s:=l.CommaText;
+    Assert(s=cComma);
+
+    l.CommaText:=cComma;
+    s:=l.Text;
+    Assert(s=cMulti+#13#10);
+  finally
+    Sys.FreeAndNil(l);
+  end;
 end;
 
 initialization
 
-  TIdTest.RegisterTest(TIdTestNativeComponent);
+  TIdTest.RegisterTest(TIdTestStringList);
+  //TIdTest.RegisterTest(TIdTestNativeComponent);
   TIdTest.RegisterTest(TIdTestStringStream);
 
 end.
