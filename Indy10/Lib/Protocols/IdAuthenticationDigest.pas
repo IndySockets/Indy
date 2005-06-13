f01b28cdfa295b24ae53f8e4edf9cf353eb8816e
Ben Taylor <indy@svn.indy.org>
2005-06-13 13:25:24 +0000

    Updated File Header

--- Indy10/Lib/Protocols/IdAuthenticationDigest.pas
+++ Indy10/Lib/Protocols/IdAuthenticationDigest.pas
@@ -1,13 +1,20 @@
-{ $HDR$}
-{**********************************************************************}
-{ Unit archived using Team Coherence                                   }
-{ Team Coherence is Copyright 2002 by Quality Software Components      }
-{                                                                      }
-{ For further information / comments, visit our WEB site at            }
-{ http://www.TeamCoherence.com                                         }
-{**********************************************************************}
-{}
-{ $Log:  13736: IdAuthenticationDigest.pas
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
 {
   2005-04-22 BTaylor
   Fixed AV from incorrect object being freed
