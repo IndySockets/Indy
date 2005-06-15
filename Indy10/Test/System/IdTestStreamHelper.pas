f4d9a2859e2073899d6815d410bddad7299072bb
Ben Taylor <indy@svn.indy.org>
2005-06-15 12:27:26 +0000

    Fixed bug. Added more checks.

--- Indy10/Test/System/IdTestStreamHelper.pas
+++ Indy10/Test/System/IdTestStreamHelper.pas
@@ -21,14 +21,20 @@ implementation
 procedure TIdTestStreamHelper.TestHelper;
 var
  aStream:TIdMemoryStream;
- aBuffer,aBuffer2:TIdBytes;
+ aBuffer:TIdBytes;
  aStr:string;
+ aCount:Integer;
 const
  cText='0123456789';
 begin
 
  aStream:=TIdMemoryStream.Create;
  try
+
+ //should be safe with nil buffer and 0 count
+ Assert(aBuffer=nil);
+ aCount:=TIdStreamHelper.ReadBytes(aStream,aBuffer,0);
+ Assert(aCount=0);
 
  //text to buffer
  aBuffer:=ToBytes(cText);
@@ -40,10 +46,19 @@ begin
  aStream.Position:=0;
  aStr:=ReadStringFromStream(aStream);
  Assert(aStr=cText);
+ SetLength(aBuffer,0);
+
+ //stream to buffer, positioned at eof
+ aStream.Position:=aStream.Size;
+ aCount:=TIdStreamHelper.ReadBytes(aStream,aBuffer);
+ Assert(aBuffer=nil);
+ Assert(aCount=0);
 
  //stream to buffer
- TIdStreamHelper.ReadBytes(aStream,aBuffer2);
+ aStream.Position:=0;
+ aCount:=TIdStreamHelper.ReadBytes(aStream,aBuffer);
  aStr:=BytesToString(aBuffer);
+ Assert(aCount=Length(cText));
  Assert(aStr=cText);
 
  //test read offsets
