04a4ae221fb694083772f10c86545f7f45f1d113
Matthijs ter Woord <indy@svn.indy.org>
2005-06-17 16:45:17 +0000

    (empty log message)

---
+++ Indy10/Test/System/IdTestBaseComponent.pas
@@ -0,0 +1,36 @@
+unit IdTestBaseComponent;
+
+interface
+
+uses
+  IdTest,
+  IdBaseComponent,
+  IdSys;
+
+type
+
+  TIdTestInitializerComponent = class(TIdTest)
+  published
+    procedure TestInitializerComponent;
+  end;
+
+implementation
+
+procedure TIdTestInitializerComponent.TestInitializerComponent;
+var
+	c: TIdInitializerComponent;
+begin
+	c := TIdInitializerComponent.Create;
+	try
+		Assert(c.Owner = nil);
+	finally
+		sys.FreeAndNil(c);
+	end;
+end;
+
+
+initialization
+
+  TIdTest.RegisterTest(TIdTestInitializerComponent);
+
+end.
