30d2bd247c438909fa01e15eb92e87e6e5072575
Ben Taylor <indy@svn.indy.org>
2005-06-18 10:47:32 +0000

    (empty log message)

---
+++ Indy10/Test/Core/IdTestReply.pas
@@ -0,0 +1,23 @@
+unit IdTestReply;
+
+//See TIdTestReplyRFC, as IdReply has abstract functions
+
+interface
+
+uses
+  IdGlobal,
+  IdReply,
+  IdSys,
+  IdTest;
+
+type
+
+  TIdTestReply = class(TIdTest)
+  published
+  end;
+
+implementation
+
+initialization
+
+end.
