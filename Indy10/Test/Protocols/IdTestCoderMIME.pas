80c8b5d7b61569b11f6b1334441ee949be66d94a
Matthijs ter Woord <indy@svn.indy.org>
2005-06-18 15:05:15 +0000

    Tried to fix

--- Indy10/Test/Protocols/IdTestCoderMIME.pas
+++ Indy10/Test/Protocols/IdTestCoderMIME.pas
@@ -5,6 +5,8 @@ interface
 uses
   IdCoderMIME,
   IdSys,
+  IdGlobal,
+  IdObjs,
   IdTest;
 
 type
@@ -18,8 +20,8 @@ type
 implementation
 
 const
- cEnc1:string='TWFya3RlaW5m/GhydW5nIGVpbmVzIHb2bGxpZyBuZXVlbiBLb256ZXB0cw==';
- cDec1='Markteinführung eines völlig neuen Konzepts';
+ cEnc1: string='TWFya3RlaW5m/GhydW5nIGVpbmVzIHb2bGxpZyBuZXVlbiBLb256ZXB0cw==';
+ cDec1 = 'Markteinführung eines völlig neuen Konzepts';
 
 procedure TIdTestCoderMIME.TestDecodeMIME;
 var
@@ -31,10 +33,28 @@ end;
 
 procedure TIdTestCoderMIME.TestDecodeLineByLine;
 var
- s:string;
+ s: string;
+ d: TIdDecoderMIMELineByLine;
+ TempStream: TIdMemoryStream;
 begin
  s:=TIdDecoderMIMELineByLine.DecodeString(cEnc1);
  Assert(s=cDec1,s);
+ d := TIdDecoderMIMELineByLine.Create;
+ try
+  TempStream := TIdMemoryStream.Create;
+  try
+   d.DecodeBegin(TempStream);
+   d.Decode(cEnc1);
+   TempStream.Position := 0;
+   s := ReadStringFromStream(TempStream);
+   Assert(s = cDec1, Sys.Format('Is "%s", should be "%s"', [s, cDec1]));
+  finally
+   d.DecodeEnd;
+   Sys.FreeAndNil(TempStream);
+  end;
+ finally
+  Sys.FreeAndNil(d);
+ end;
 end;
 
 initialization
