bbc95e2fce379b4c7b16c0ab2b3ea1373fb71440
Ben Taylor <indy@svn.indy.org>
2005-06-18 09:00:33 +0000

    Modified for win32. Fixed Assert.

--- Indy10/Test/Core/IdTestCmdTCPServer.pas
+++ Indy10/Test/Core/IdTestCmdTCPServer.pas
@@ -24,7 +24,6 @@ procedure TIdTestCmdTCPServer.TestServer;
 var
 	aClient: TIdTCPClient;
 	aServer: TIdCmdTCPServer;
-	o: &Object;
 const
 	cTestPort=20202;
 	cGreetingText = 'Hello There';
@@ -32,10 +31,6 @@ begin
 	aClient:= TIdTCPClient.Create(nil);
 	aServer:= TIdCmdTCPServer.Create(nil);
 	try
-		aClient.IOHandler := TIdIOHandlerStack.Create;
-		aClient.IOHandler.Intercept := TIdLogDebug.Create;
-		TIdLogDebug(AClient.IOHandler.Intercept).Active := true;
-		try
 			aServer.DefaultPort:=cTestPort;
 			aServer.Greeting.Text.Text := cGreetingText;
 			aServer.Greeting.Code := '200';
@@ -44,15 +39,8 @@ begin
 			aClient.Host:='127.0.0.1';
 			aClient.Connect;
 			Assert(aClient.GetResponse(200) = 200);
-			Console.WriteLine('LastCmdResult.Text = "{0}"', AClient.LastCmdResult.Text.Text);
-			Assert(aClient.LastCmdResult.Text.Text = cGreetingText);
+			Assert(aClient.LastCmdResult.Text.Text = cGreetingText+EOL);
 			aClient.Disconnect;
-		finally
-			o := AClient.IOHandler.Intercept;
-			Sys.FreeAndNil(o);
-			o := AClient.IOHandler;
-			Sys.FreeAndNil(o);
-		end;
 	finally
 		Sys.FreeAndNil(aClient);
 		Sys.FreeAndNil(aServer);
