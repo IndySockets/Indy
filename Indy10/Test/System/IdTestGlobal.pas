78c5b150dbb2b1e0968c5e59a11ff1f6339d6486
Ben Taylor <indy@svn.indy.org>
2005-06-19 02:32:28 +0000

    Added char tests

--- Indy10/Test/System/IdTestGlobal.pas
+++ Indy10/Test/System/IdTestGlobal.pas
@@ -11,18 +11,56 @@ type
   TIdTestGlobal = class(TIdTest)
   published
     procedure TestToBytes;
+    procedure TestBytesToChar;
   end;
 
 implementation
 
+procedure TIdTestGlobal.TestBytesToChar;
+var
+  aBytes:TIdBytes;
+  aChar:char;
+begin
+  //test normal behaviour
+  SetLength(aBytes,1);
+  aBytes[0]:=$31;
+  aChar:=BytesToChar(aBytes);
+  Assert(aChar='1');
+
+  //should fail trying to read from empty buffer
+  SetLength(aBytes,0);
+  try
+  BytesToChar(aBytes);
+  Assert(False);
+  except
+  //expect to be here
+  end;
+
+  //should fail trying to read from nil buffer
+  aBytes:=nil;
+  try
+  BytesToChar(aBytes);
+  Assert(False);
+  except
+  //expect to be here
+  end;
+
+end;
+
 procedure TIdTestGlobal.TestToBytes;
 const
   cTestString1 = 'U';
+  cTestChar = 'U';
 var
   aBytes : TIdBytes;
 begin
   //test string
   aBytes := ToBytes(cTestString1);
+  Assert(Length(aBytes)=1);
+  Assert(aBytes[0] = 85);
+
+  //test char
+  aBytes := ToBytes(cTestChar);
   Assert(Length(aBytes)=1);
   Assert(aBytes[0] = 85);
 
