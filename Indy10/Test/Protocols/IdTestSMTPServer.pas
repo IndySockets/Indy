369b37dfde65bb1c82004b38856278df1cb40853
Matthijs ter Woord <indy@svn.indy.org>
2005-06-19 12:26:31 +0000

    Adjusted outlining

--- Indy10/Test/Protocols/IdTestSMTPServer.pas
+++ Indy10/Test/Protocols/IdTestSMTPServer.pas
@@ -13,7 +13,7 @@ uses
   IdObjs,
   IdSMTP,
   IdSys,
-  IdLogFile,
+  IdLogDebug,
   IdTest;
 
 type
@@ -45,197 +45,191 @@ const
 
 procedure TIdTestSMTPServer.mySetup;
 begin
- Assert(FReceivedMsg=nil);
- FReceivedMsg:=TIdMessage.Create(nil);
+  Assert(FReceivedMsg=nil);
+  FReceivedMsg:=TIdMessage.Create(nil);
 
- Assert(FServer=nil);
- FServer:=TIdSMTPServer.Create(nil);
- FServer.OnMsgReceive:=CallbackMsgReceive;
- FServer.OnRcptTo:=CallbackRcptTo;
- FServer.OnMailFrom:=CallbackMailFrom;
- FServer.DefaultPort:=cTestPort;
+  Assert(FServer=nil);
+  FServer:=TIdSMTPServer.Create(nil);
+  FServer.OnMsgReceive:=CallbackMsgReceive;
+  FServer.OnRcptTo:=CallbackRcptTo;
+  FServer.OnMailFrom:=CallbackMailFrom;
+  FServer.DefaultPort:=cTestPort;
 
- Assert(FClient=nil);
- FClient:=TIdSMTP.Create(nil);
- FClient.ReadTimeout:=5000;
- FClient.Port:=cTestPort;
- FClient.Host:='127.0.0.1';
- FClient.CreateIOHandler;
- FClient.IOHandler.Intercept := TIdLogFile.Create;
- TIdLogFile(FClient.IOHandler.Intercept).FileName := 'tcp.log';
- TIdLogFile(FClient.IOHandler.Intercept).Open;
-
+  Assert(FClient=nil);
+  FClient:=TIdSMTP.Create(nil);
+  FClient.ReadTimeout:=5000;
+  FClient.Port:=cTestPort;
+  FClient.Host:='127.0.0.1';
+  FClient.CreateIOHandler;
+  FClient.IOHandler.Intercept := TIdLogDebug.Create;
+  TIdLogDebug(FClient.IOHandler.Intercept).Active := True;
 end;
 
 procedure TIdTestSMTPServer.myTearDown;
 begin
- TIdLogFile(FClient.IOHandler.Intercept).Close;
- Sys.FreeAndNil(FClient);
- Sys.FreeAndNil(FServer);
- Sys.FreeAndNil(FReceivedMsg);
+  TIdLogDebug(FClient.IOHandler.Intercept).Active := False;
+  Sys.FreeAndNil(FClient);
+  Sys.FreeAndNil(FServer);
+  Sys.FreeAndNil(FReceivedMsg);
 end;
 
 procedure TIdTestSMTPServer.CallbackMailFrom(ASender: TIdSMTPServerContext;
   const AAddress: string; var VAction: TIdMailFromReply);
 begin
- if AAddress=cSpammerAddress then VAction:=mReject;
+  if AAddress=cSpammerAddress then VAction:=mReject;
 end;
 
 procedure TIdTestSMTPServer.CallbackMsgReceive(
   ASender: TIdSMTPServerContext; AMsg: TIdStream2;
   var LAction: TIdDataReply);
 var
- aList:TIdStringList;
+  aList:TIdStringList;
 begin
+  //do a precheck here.
+  aList:=TIdStringList.Create;
+  try
+    AMsg.Position:=0;
+    aList.Text:=ReadStringFromStream(AMsg);
+    //should be at least headers for: received from subject to date
+    //if this fails, then client hasn't written correctly, or server hasn't read
+    Assert(aList.Count>=5);
+  finally
+    sys.FreeAndNil(aList);
+  end;
 
- //do a precheck here.
- aList:=TIdStringList.Create;
- try
- AMsg.Position:=0;
- aList.Text:=ReadStringFromStream(AMsg);
- //should be at least headers for: received from subject to date
- //if this fails, then client hasn't written correctly, or server hasn't read
- Assert(aList.Count>=5);
- finally
- sys.FreeAndNil(aList);
- end;
-
- AMsg.Position:=0;
- FReceivedMsg.LoadFromStream(AMsg);
+  AMsg.Position:=0;
+  FReceivedMsg.LoadFromStream(AMsg);
 end;
 
 procedure TIdTestSMTPServer.CallbackRcptTo(ASender: TIdSMTPServerContext;
   const AAddress: string; var VAction: TIdRCPToReply;
   var VForward: string);
 begin
- VAction:=rAddressOk;
+  VAction:=rAddressOk;
 end;
 
 procedure TIdTestSMTPServer.TestGreeting;
 //checks that the server returns a correct greeting
 //uses tcpclient to check directly
 var
- s:string;
- aClient:TIdTCPClient;
+  s:string;
+  aClient:TIdTCPClient;
 const
- cGreeting='HELLO';
+  cGreeting='HELLO';
 begin
- aClient:=TIdTCPClient.Create(nil);
- try
- mySetup;
+  aClient:=TIdTCPClient.Create(nil);
+  try
+    mySetup;
 
- //Set a specific greeting, makes easy to test
- (FServer.Greeting as TIdReplySMTP).SetEnhReply(220, '' ,cGreeting);
- FServer.Active:=True;
+    //Set a specific greeting, makes easy to test
+    (FServer.Greeting as TIdReplySMTP).SetEnhReply(220, '' ,cGreeting);
+    FServer.Active:=True;
 
- aClient.Port:=cTestPort;
- aClient.Host:='127.0.0.1';
- aClient.ReadTimeout:=5000;
- aClient.Connect;
+    aClient.Port:=cTestPort;
+    aClient.Host:='127.0.0.1';
+    aClient.ReadTimeout:=5000;
+    aClient.Connect;
 
- s:=aClient.IOHandler.Readln;
- //real example '220 mail.example.com ESMTP'
- Assert(s='220 '+cGreeting);
- aClient.Disconnect;
-
- finally
- Sys.FreeAndNil(aClient);
- myTearDown;
- end;
+    s:=aClient.IOHandler.Readln;
+    //real example '220 mail.example.com ESMTP'
+    Assert(s='220 '+cGreeting);
+    aClient.Disconnect;
+  finally
+    Sys.FreeAndNil(aClient);
+    myTearDown;
+  end;
 end;
 
 procedure TIdTestSMTPServer.TestReceive;
 //do a round-trip message send using smtp client and server
 //repeat with/without client pipelining?
 var
- aMessage:TIdMessage;
+  aMessage:TIdMessage;
 const
- cFrom='bob@example.com';
- cSubject='mysubject';
- cBody='1,2,3';
- cAddress='mary@example.com';
- cPriority:TIdMessagePriority=mpHigh;
+  cFrom='bob@example.com';
+  cSubject='mysubject';
+  cBody='1,2,3';
+  cAddress='mary@example.com';
+  cPriority:TIdMessagePriority=mpHigh;
 begin
- try
- mySetup;
+  try
+    mySetup;
 
- FServer.Active:=True;
- FClient.Connect;
+    FServer.Active:=True;
+    FClient.Connect;
 
- aMessage:=TIdMessage.Create(nil);
- try
- aMessage.From.Address:=cFrom;
- aMessage.Subject:=cSubject;
- aMessage.Body.CommaText:=cBody;
- //test with multiple recipients
- aMessage.Recipients.Add.Address:=cAddress;
- aMessage.Priority:=cPriority;
- FClient.Send(aMessage);
- finally
- Sys.FreeAndNil(aMessage);
- end;
+    aMessage := TIdMessage.Create(nil);
+    try
+      aMessage.From.Address := cFrom;
+      aMessage.Subject := cSubject;
+      aMessage.Body.CommaText := cBody;
+      //test with multiple recipients
+      aMessage.Recipients.Add.Address := cAddress;
+      aMessage.Priority :=cPriority;
+      aMessage.Encoding := mePlaintext;
+      FClient.Send(aMessage);
+    finally
+      Sys.FreeAndNil(aMessage);
+    end;
 
- Assert(FClient.LastCmdResult.NumericCode=250);
+    Assert(FClient.LastCmdResult.NumericCode =250, 'Code:' + Sys.IntToStr(FClient.LastCmdResult.NumericCode));
 
- //check that what the server received is same as sent
- Assert(FReceivedMsg.From.Address=cFrom);
- Assert(FReceivedMsg.Subject=cSubject);
- Assert(FReceivedMsg.Body.CommaText=cBody);
- Assert(FReceivedMsg.Recipients.EMailAddresses=cAddress);
- Assert(FReceivedMsg.Priority=cPriority);
- //also check the "Received:" header
- //also check attachments
+    //check that what the server received is same as sent
+    Assert(FReceivedMsg.From.Address = cFrom, 'From:' + FReceivedMsg.From.Address);
+    Assert(FReceivedMsg.Subject = cSubject, 'Subject:' + FReceivedMsg.Subject);
+    Assert(FReceivedMsg.Body.CommaText = cBody, 'Body:' + FReceivedMsg.Body.CommaText);
+    Assert(FReceivedMsg.Recipients.EMailAddresses = cAddress, 'To:' + FReceivedMsg.Recipients.EMailAddresses);
+    Assert(FReceivedMsg.Priority = cPriority, 'Priority');
+    //also check the "Received:" header
+    //also check attachments
 
- //currently breaks due to ioHandler rev26
- finally
- myTearDown;
- end;
-
+    //currently breaks due to ioHandler rev26
+  finally
+    myTearDown;
+  end;
 end;
 
 procedure TIdTestSMTPServer.TestReject;
 //check that if a message is rejected by server, (here using OnMailFrom)
 //the correct status is returned.
 var
- aMessage:TIdMessage;
+  aMessage:TIdMessage;
 begin
 
- try
- mySetup;
+  try
+    mySetup;
 
- FServer.Active:=True;
- FClient.Connect;
+    FServer.Active:=True;
+    FClient.Connect;
 
- aMessage:=TIdMessage.Create(nil);
- try
- aMessage.From.Address:=cSpammerAddress;
- aMessage.Subject:='spam';
- aMessage.Body.CommaText:='spam';
- aMessage.Recipients.Add.Address:='bob@example.com';
- try
- FClient.Send(aMessage);
- except
- //want to ignore the exception here
- //EIdSMTPReplyError
- //check class,content
- end;
- finally
- Sys.FreeAndNil(aMessage);
- end;
+    aMessage:=TIdMessage.Create(nil);
+    try
+      aMessage.From.Address:=cSpammerAddress;
+      aMessage.Subject:='spam';
+      aMessage.Body.CommaText:='spam';
+      aMessage.Recipients.Add.Address:='bob@example.com';
+      try
+        FClient.Send(aMessage);
+      except
+        //want to ignore the exception here
+        //EIdSMTPReplyError
+        //check class,content
+      end;
+    finally
+      Sys.FreeAndNil(aMessage);
+    end;
 
 
- //TIdSMTPServer.MailFromReject sets to 250
- //should be 550
- //currently responding 503, from TIdSMTPServer.BadSequenceError
- Assert(FClient.LastCmdResult.NumericCode=550, FClient.LastCmdResult.FormattedReply.Text);
- //Assert(aClient.LastCmdResult.Code='550');
- //Bad sequence of commands
- //Assert(aClient.LastCmdResult.Text.Text='');
-
- finally
- myTearDown;
- end;
-
+    //TIdSMTPServer.MailFromReject sets to 250
+    //should be 550
+    //currently responding 503, from TIdSMTPServer.BadSequenceError
+    Assert(FClient.LastCmdResult.NumericCode = 550, FClient.LastCmdResult.FormattedReply.Text);
+    //Assert(aClient.LastCmdResult.Code = '550');
+    //Bad sequence of commands
+    //Assert(aClient.LastCmdResult.Text.Text = '');
+  finally
+    myTearDown;
+  end;
 end;
 
 initialization
