79e80048cddd6918578170e5e3676729d6679c18
Ben Taylor <indy@svn.indy.org>
2005-06-22 07:41:31 +0000

    (empty log message)

---
+++ Indy10/Other/Protocols/IdCompressorAbbrevia.pas
@@ -0,0 +1,67 @@
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
+
+unit IdCompressorAbbrevia;
+
+//uses the opensource Abbrevia component set to implement compression
+//eg for http gzip decoding
+//see http://sourceforge.net/projects/tpabbrevia/
+
+interface
+
+uses
+  AbGzTyp,
+  AbUtils,
+  IdZLibCompressorBase,
+  IdObjs,
+  IdSys;
+
+type
+
+  //currently just implements gzip decompression. 
+  TIdCompressorAbbrevia = class(TIdZLibCompressorBase)
+  public
+    procedure DecompressGZipStream(AStream : TIdStream2; const AOutStream : TIdStream2=nil); override;
+  end;
+
+implementation
+
+procedure TIdCompressorAbbrevia.DecompressGZipStream(AStream: TIdStream2; const AOutStream: TIdStream2);
+var
+  aGz:TAbGzipStreamHelper;
+  aDest:TIdMemoryStream;
+  aType:TAbArchiveType;
+begin
+  //no inherited;
+
+  aType:=VerifyGZip(AStream);
+  Assert(aType=atGzip);
+
+  aGz:=TAbGzipStreamHelper.Create(aStream);
+  aDest:=TIdMemoryStream.Create;
+  try
+  aGz.ExtractItemData(aDest);
+  AStream.Size:=0;
+  AStream.CopyFrom(aDest,0);
+  finally
+  Sys.FreeAndNil(aGz);
+  Sys.FreeAndNil(aDest);
+  end;
+end;
+
+end.
