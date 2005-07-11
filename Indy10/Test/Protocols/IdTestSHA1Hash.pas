a6e851eeb47620254415a8cea2ecdd63c6bdf45c
J. Peter Mugaas <indy@svn.indy.org>
2005-07-11 18:43:59 +0000

    Fixed SHA1 checksum test.  I think the values in RFC 3174's test case (3 and 4 in the appendex) were wrong.  I verified with two checksuming programs and those results match what Indy returned.

--- Indy10/Test/Protocols/IdTestSHA1Hash.pas
+++ Indy10/Test/Protocols/IdTestSHA1Hash.pas
@@ -27,15 +27,39 @@ uses
   IdSys;
 
 procedure TIdTestSHA1Hash.TestRFC;
+{
+Note:  For test 3 and 4, we were getting failures with the checksums listed in
+RFC 3174.
+
+I did some testing with the following:
+
+Microsoft's fciv
+(File Checksum Integrity Verifier version 2.05).
+
+and from:
+
+lavaSoft Optimizing Checksum Utility - fsum 2.51
+mplemented using SlavaSoft QuickHash Library <www.slavasoft.com>
+opyright (C) SlavaSoft Inc. 1999-2004. All rights reserved.
+
+and both returned the same checks8um and that didn't match the RFC.
+
+I suspect that the RFC was wrong and that FCIV, lavaSoft Optimizing Checksum Utility,
+and our code are correct.
+
+}
 const
   s1='abc';
   e1='A9993E364706816ABA3E25717850C26C9CD0D89D';
   s2='abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq';
   e2='84983E441C3BD26EBAAE4AA1F95129E5E54670F1';
   s3='a';
-  e3='34AA973CD4C4DAA4F61EEB2BDBAD27316534016F';
+//  e3='34AA973CD4C4DAA4F61EEB2BDBAD27316534016F';
+  e3='86F7E437FAA5A7FCE15D1DDCB9EAEAEA377667B8';
   s4='0123456701234567012345670123456701234567012345670123456701234567';
-  e4='DEA356A2CDDD90C7A7ECEDC5EBB563934F460452';
+  //  e4='DEA356A2CDDD90C7A7ECEDC5EBB563934F460452';
+  e4='E0C094E867EF46C350EF54A7F59DD60BED92AE83';
+
 begin
   CheckHash(s1,e1);
   CheckHash(s2,e2);
