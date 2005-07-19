fd664c77acd614ed1cd7a5aad086f3641705c497
Don Siders <indy@svn.indy.org>
2005-07-19 19:50:30 +0000

    Added the standard file header. Added links to the HMAC specs at NIST.

--- Indy10/Lib/Protocols/IdHMACMD5.pas
+++ Indy10/Lib/Protocols/IdHMACMD5.pas
@@ -1,3 +1,24 @@
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
+{
+  HMAC specification on the NIST website
+  http://csrc.nist.gov/publications/fips/fips198/fips-198a.pdf
+}
 unit IdHMACMD5;
 
 interface
--- Indy10/Lib/Protocols/IdHMACSHA1.pas
+++ Indy10/Lib/Protocols/IdHMACSHA1.pas
@@ -1,3 +1,24 @@
+{
+  $Project$
+  $Workfile$
+  $Revision$
+  $DateUTC$
+  $Id$
+
+  This file is part of the Indy (Internet Direct) project, and is offered
+  under the dual-licensing agreement described on the Indy website.
+  (http://www.indyproject.org/)
+
+  Copyright:
+   (c) 1993-2005, Chad Z. Hower and the Indy Pit Crew. All rights reserved.
+}
+{
+  $Log$
+}
+{
+  HMAC specification on the NIST website
+  http://csrc.nist.gov/publications/fips/fips198/fips-198a.pdf
+}
 unit IdHMACSHA1;
 
 interface
@@ -13,7 +34,7 @@ type
     procedure InitHash; override;
     function InternalHashValue(const ABuffer: TIdBytes) : TIdBytes; override;
   end;
-  
+
 implementation
 
 { TIdHMACSHA1 }
@@ -25,7 +46,7 @@ begin
   FHash := FHashSHA1;
   FHashSize := 20;
   FBlockSize := 64;
-  FHashName := 'SHA1';  
+  FHashName := 'SHA1';
 end;
 
 
