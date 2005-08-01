b55c5ec6571e401e36b479f17eccaaf9b97ea443
Ben Taylor <indy@svn.indy.org>
2005-08-01 07:10:03 +0000

    TIdTestBuffer.TestTypes

--- Indy10/Test/Core/IdTestBuffer.pas
+++ Indy10/Test/Core/IdTestBuffer.pas
@@ -15,6 +15,7 @@ type
   published
     procedure TestBufferToStream;
     procedure TestStreamToBuffer;
+    procedure TestTypes;
   end;
 
 implementation
@@ -96,6 +97,103 @@ begin
  Sys.FreeAndNil(aStream);
  Sys.FreeAndNil(aBuffer);
  end;
+end;
+
+procedure TIdTestBuffer.TestTypes;
+var
+  b:TIdBuffer;
+  //aInt:Integer;
+  i64:Int64;
+  aCardinal:Cardinal;
+  aWord:Word;
+  aByte:Byte;
+const
+  //cInteger=2000000000;
+  cCardinal=4000000000;
+  cCardinal2=4000000001;
+  c64=5000000000;
+  c642=5000000001;
+  cWord=60000;
+  cWord2=60001;
+  cByte=250;
+  cByte2=255;
+begin
+  b:=TIdBuffer.Create;
+  try
+
+  {
+  //integer currently gets sent to the int64 overload
+  aInt:=cInteger;
+  b.Write(aInt);
+  aInt:=Integer(b.ExtractToCardinal(0));
+  Assert(aInt=cInteger);
+  Assert(b.Size=0);
+  }
+
+  //write 2 different bytes
+  aByte:=cByte;
+  b.Write(aByte);
+  Assert(b.Size=SizeOf(Byte));
+  aByte:=cByte2;
+  b.Write(aByte);
+  Assert(b.Size=2*SizeOf(Byte));
+  //read the 2nd byte. index>-1 so doesnt actually extract
+  aByte:=b.ExtractToByte(SizeOf(Byte));
+  Assert(b.Size=2*SizeOf(Byte));
+  Assert(aByte=cByte2);
+  //and extract the data, check expected order (FIFO)
+  aByte:=b.ExtractToByte(-1);
+  Assert(aByte=cByte);
+  Assert(b.Size=SizeOf(Byte));
+  aByte:=b.ExtractToByte(-1);
+  Assert(aByte=cByte2);
+  Assert(b.Size=0);
+
+  aCardinal:=cCardinal;
+  b.Write(aCardinal);
+  aCardinal:=cCardinal2;
+  b.Write(aCardinal);
+  Assert(b.Size=2*SizeOf(Cardinal));
+  aCardinal:=b.ExtractToCardinal(SizeOf(Cardinal));
+  Assert(b.Size=2*SizeOf(Cardinal));
+  Assert(aCardinal=cCardinal2);
+  aCardinal:=b.ExtractToCardinal(-1);
+  Assert(aCardinal=cCardinal);
+  aCardinal:=b.ExtractToCardinal(-1);
+  Assert(aCardinal=cCardinal2);
+  Assert(b.Size=0);
+
+  i64:=c64;
+  b.Write(i64);
+  i64:=c642;
+  b.Write(i64);
+  Assert(b.Size=2*SizeOf(Int64));
+  i64:=b.ExtractToInt64(SizeOf(Int64));
+  Assert(i64=c642);
+  Assert(b.Size=2*SizeOf(Int64));
+  i64:=b.ExtractToInt64(-1);
+  Assert(i64=c64);
+  i64:=b.ExtractToInt64(-1);
+  Assert(i64=c642);
+  Assert(b.Size=0);
+
+  aWord:=cWord;
+  b.Write(aWord);
+  aWord:=cWord2;
+  b.Write(aWord);
+  Assert(b.Size=2*SizeOf(Word));
+  aWord:=b.ExtractToWord(SizeOf(Word));
+  Assert(aWord=cWord2);
+  Assert(b.Size=2*SizeOf(Word));
+  aWord:=b.ExtractToWord(-1);
+  Assert(aWord=cWord);
+  aWord:=b.ExtractToWord(-1);
+  Assert(aWord=cWord2);
+  Assert(b.Size=0);
+
+  finally
+  Sys.FreeAndNil(b);
+  end;
 end;
 
 initialization
