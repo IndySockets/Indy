59d1091837f250ff08e19db62a4bc126f719a7e8
J. Peter Mugaas <indy@svn.indy.org>
2005-08-12 09:28:41 +0000

    (empty log message)

---
+++ Indy10/Lib/Protocols/DelphiZLib.cpp
@@ -0,0 +1,7 @@
+#include <condefs.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+#define Library
+
+// To add a file to the library use the Project menu 'Add to Project'.
+
