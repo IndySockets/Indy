e4adcde2f473be6ed9f0b24ce225a83e6e46f8e7
J. Peter Mugaas <indy@svn.indy.org>
2005-08-29 23:31:20 +0000

    (empty log message)

---
+++ Indy10/Lib/Protocols/IdZLibConst.pas
@@ -0,0 +1,21 @@
+
+{*******************************************************}
+{                                                       }
+{       Borland Delphi Visual Component Library         }
+{                                                       }
+{  Copyright (c) 1997-1999 Borland Software Corporation }
+{                                                       }
+{*******************************************************}
+
+unit IdZLibConst;
+
+interface
+
+resourcestring
+ sTargetBufferTooSmall = 'ZLib error: target buffer may be too small';
+ sInvalidStreamOp = 'Invalid stream operation';
+  
+implementation
+
+end.
+ 
