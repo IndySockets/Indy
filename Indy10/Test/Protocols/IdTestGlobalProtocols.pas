d8b6ac00010c0c0c17b87e4989100d7e8702c122
Ben Taylor <indy@svn.indy.org>
2005-12-03 01:14:04 +0000

    Added TIdMimeTable

--- Indy10/Test/Protocols/IdTestGlobalProtocols.pas
+++ Indy10/Test/Protocols/IdTestGlobalProtocols.pas
@@ -15,6 +15,7 @@ type
   published
     procedure TestRPos;
     procedure TestStrToWord;
+    procedure TestMimeTable;
   end;
 
 implementation
@@ -29,6 +30,19 @@ procedure TIdTestGlobalProtocols.TestRPos;
 begin
   Assert(RPos(' ', 'Hello, World!', -1) = 7);
   Assert(RPos('_', 'Hello, World!', -1) = 0);
+end;
+
+procedure TIdTestGlobalProtocols.TestMimeTable;
+var
+  m:TIdMimeTable;
+begin
+  m:=TIdMimeTable.Create(False);
+  try
+    m.LoadTypesFromOS:=True;
+    m.BuildCache;
+  finally
+    Sys.FreeAndNil(m);
+  end;
 end;
 
 initialization
