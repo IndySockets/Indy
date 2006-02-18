471ae9d874e1ccdb047325e0a4e1fb1208bed07d
Don Siders <indy@svn.indy.org>
2006-02-18 04:50:27 +0000

    Clean-up of old revision comments.

--- Indy10/Lib/Protocols/IdTimeUDP.pas
+++ Indy10/Lib/Protocols/IdTimeUDP.pas
@@ -16,36 +16,30 @@
   $Log$
 }
 {
-{   Rev 1.7    2/10/2005 2:24:38 PM  JPMugaas
-{ Minor Restructures for some new UnixTime Service components.
+  Rev 1.7    2/10/2005 2:24:38 PM  JPMugaas
+  Minor Restructures for some new UnixTime Service components.
+
+  Rev 1.6    2004.02.03 5:44:36 PM  czhower
+  Name changes
+
+  Rev 1.5    1/21/2004 4:21:00 PM  JPMugaas
+  InitComponent
+
+  Rev 1.4    1/3/2004 1:00:06 PM  JPMugaas
+  These should now compile with Kudzu's change in IdCoreGlobal.
+
+  Rev 1.3    10/26/2003 5:16:52 PM  BGooijen
+  Works now, times in GetTickDiff were in wrong order
+
+  Rev 1.2    10/22/2003 04:54:26 PM  JPMugaas
+  Attempted to get these to work.
+
+  Rev 1.1    2003.10.12 6:36:46 PM  czhower
+  Now compiles.
+
+  Rev 1.0    11/13/2002 08:03:24 AM  JPMugaas
 }
-{
-{   Rev 1.6    2004.02.03 5:44:36 PM  czhower
-{ Name changes
-}
-{
-{   Rev 1.5    1/21/2004 4:21:00 PM  JPMugaas
-{ InitComponent
-}
-{
-{   Rev 1.4    1/3/2004 1:00:06 PM  JPMugaas
-{ These should now compile with Kudzu's change in IdCoreGlobal.
-}
-{
-{   Rev 1.3    10/26/2003 5:16:52 PM  BGooijen
-{ Works now, times in GetTickDiff were in wrong order
-}
-{
-{   Rev 1.2    10/22/2003 04:54:26 PM  JPMugaas
-{ Attempted to get these to work.
-}
-{
-{   Rev 1.1    2003.10.12 6:36:46 PM  czhower
-{ Now compiles.
-}
-{
-{   Rev 1.0    11/13/2002 08:03:24 AM  JPMugaas
-}
+
 unit IdTimeUDP;
 
 interface
--- Indy10/Lib/Protocols/IdTimeUDPServer.pas
+++ Indy10/Lib/Protocols/IdTimeUDPServer.pas
@@ -16,28 +16,24 @@
   $Log$
 }
 {
-{   Rev 1.6    2/10/2005 2:24:38 PM  JPMugaas
-{ Minor Restructures for some new UnixTime Service components.
+  Rev 1.6    2/10/2005 2:24:38 PM  JPMugaas
+  Minor Restructures for some new UnixTime Service components.
+
+  Rev 1.5    2004.02.03 5:44:36 PM  czhower
+  Name changes
+
+  Rev 1.4    1/21/2004 4:21:02 PM  JPMugaas
+  InitComponent
+
+  Rev 1.3    10/22/2003 08:48:06 PM  JPMugaas
+  Minor code cleanup.
+
+  Rev 1.1    2003.10.12 6:36:46 PM  czhower
+  Now compiles.
+
+  Rev 1.0    11/13/2002 08:03:28 AM  JPMugaas
 }
-{
-{   Rev 1.5    2004.02.03 5:44:36 PM  czhower
-{ Name changes
-}
-{
-{   Rev 1.4    1/21/2004 4:21:02 PM  JPMugaas
-{ InitComponent
-}
-{
-{   Rev 1.3    10/22/2003 08:48:06 PM  JPMugaas
-{ Minor code cleanup.
-}
-{
-{   Rev 1.1    2003.10.12 6:36:46 PM  czhower
-{ Now compiles.
-}
-{
-{   Rev 1.0    11/13/2002 08:03:28 AM  JPMugaas
-}
+
 unit IdTimeUDPServer;
 
 interface
@@ -55,7 +51,7 @@ type
    TIdTimeUDPServer = class(TIdCustomTimeUDPServer)
    published
      property DefaultPort default IdPORT_TIME;
-     {This property is used to set the Date the Time server bases it's   
+     {This property is used to set the Date the Time server bases it's
      calculations from.  If both the server and client are based from the same
      date which is higher than the original date, you can extend it beyond the
      year 2035}
@@ -89,4 +85,4 @@ begin
 end;
 
 end.
- 
+
