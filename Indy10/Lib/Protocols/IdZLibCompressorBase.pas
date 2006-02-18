b668404b7725c6f959a399c9ce373d1e74058fd2
Don Siders <indy@svn.indy.org>
2006-02-18 04:31:27 +0000

    Clean-up of old revision comments.

--- Indy10/Lib/Protocols/IdZLibCompressorBase.pas
+++ Indy10/Lib/Protocols/IdZLibCompressorBase.pas
@@ -16,121 +16,121 @@
   $Log$
 }
 {
-{   Rev 1.21    3/5/2005 3:33:54 PM  JPMugaas
-{ Fix for some compiler warnings having to do with TStream.Read being platform
-{ specific.  This was fixed by changing the Compressor API to use TIdStreamVCL
-{ instead of TStream.  I also made appropriate adjustments to other units for
-{ this. 
-}
-{
-{   Rev 1.20    3/4/2005 12:36:58 PM  JPMugaas
-{ Removed some compiler warnings.
-}
-{
-{   Rev 1.19    9/16/2004 3:24:08 AM  JPMugaas
-{ TIdFTP now compresses to the IOHandler and decompresses from the IOHandler.
-{ 
-{ Noted some that the ZLib code is based was taken from ZLibEx.
-}
-{
-{   Rev 1.18    9/12/2004 7:49:06 PM  JPMugaas
-{ Removed an abstract method that was removed from the descendant to prevent a
-{ warning.  It was part of an idea i had that hasn't yet developed due to
-{ another obsticle.
-}
-{
-{   Rev 1.17    9/11/2004 10:58:10 AM  JPMugaas
-{ FTP now decompresses output directly to the IOHandler.
-}
-{
-    Rev 1.16    7/18/2004 3:01:44 PM  DSiders
+  Rev 1.21    3/5/2005 3:33:54 PM  JPMugaas
+  Fix for some compiler warnings having to do with TStream.Read being platform
+  specific.  This was fixed by changing the Compressor API to use TIdStreamVCL
+  instead of TStream.  I also made appropriate adjustments to other units for
+  this.
+
+
+  Rev 1.20    3/4/2005 12:36:58 PM  JPMugaas
+  Removed some compiler warnings.
+
+
+  Rev 1.19    9/16/2004 3:24:08 AM  JPMugaas
+  TIdFTP now compresses to the IOHandler and decompresses from the IOHandler.
+
+  Noted some that the ZLib code is based was taken from ZLibEx.
+
+
+  Rev 1.18    9/12/2004 7:49:06 PM  JPMugaas
+  Removed an abstract method that was removed from the descendant to prevent a
+  warning.  It was part of an idea i had that hasn't yet developed due to
+  another obsticle.
+
+
+  Rev 1.17    9/11/2004 10:58:10 AM  JPMugaas
+  FTP now decompresses output directly to the IOHandler.
+
+
+  Rev 1.16    7/18/2004 3:01:44 PM  DSiders
   Added localization comments.
-}
-{
-{   Rev 1.15    6/15/2004 6:33:50 PM  JPMugaas
-{ Bug fix for RaidenFTPD and ShareIt FTP Server.  Since we now specifically
-{ look for the ZLIB headers, we pass the Window Bits value as negative
-{ DecompressFTPDeflate.  I have verified this on RaidenFTPD and ShareIt.
-{
-{ Note that there is an inconsistancy in the FTP Deflate drafts
-{
-{ http://community.roxen.com/developers/idocs/drafts/draft-preston-ftpext-deflat
-{ e-02.html
-{
-{ and
-{
-{ http://community.roxen.com/developers/idocs/drafts/draft-preston-ftpext-deflat
-{ e-00.html
-}
-{
-{   Rev 1.14    6/14/2004 6:14:42 PM  JPMugaas
-{ A fix from Bas.  RaidenFTPD 1455 (http://www.raidenftpd.com) was sending the
-{
-{  2 byte header, the compression methods flags
-{
-{ while ShareIt (www.noisette-software.com/products/windows/ShareIt/) was not
-{ doing so.
-}
-{
-{   Rev 1.13    2004.05.20 11:37:22 AM  czhower
-{ IdStreamVCL
-}
-{
-{   Rev 1.12    2/15/2004 6:56:44 AM  JPMugaas
-{ GZip decompression should now work again.
-}
-{
-{   Rev 1.11    2/15/2004 6:22:26 AM  JPMugaas
-{ Fixed some parameter errors.
-}
-{
-{   Rev 1.10    2/14/2004 9:59:48 PM  JPMugaas
-{ Reworked the API.  There is now a separate API for the Inflate_ and
-{ InflateInit2_ functions as well as separate functions for DeflateInit_ and
-{ DeflateInit2_.  This was required for FTP.  The API also includes an optional
-{ output stream for the servers.
-}
-{
-{   Rev 1.9    2/12/2004 11:35:00 PM  JPMugaas
-{ FTP Deflate preliminary support.  Work still needs to be done for upload and
-{ downloading.
-}
-{
-{   Rev 1.8    2/12/2004 11:11:24 AM  JPMugaas
-{ Added methods for HTTP Compression and decompression using RFC 1950.  I have
-{ verified these.
-}
-{
-{   Rev 1.7    2004.02.03 5:45:48 PM  czhower
-{ Name changes
-}
-{
-{   Rev 1.6    10/25/2003 06:52:26 AM  JPMugaas
-{ Updated for new API changes and tried to restore some functionality.
-}
-{
-{   Rev 1.5    10/24/2003 05:04:54 PM  JPMugaas
-{ SHould work as before.
-}
-{
-{   Rev 1.4    2003.10.24 10:43:14 AM  czhower
-{ TIdSTream to dos
-}
-{
-{   Rev 1.3    10/7/2003 10:07:08 PM  GGrieve
-{ Get HTTP compiling for DotNet
-}
-{
-    Rev 1.2    7/13/2003 10:57:30 PM  BGooijen
+
+
+  Rev 1.15    6/15/2004 6:33:50 PM  JPMugaas
+  Bug fix for RaidenFTPD and ShareIt FTP Server.  Since we now specifically
+  look for the ZLIB headers, we pass the Window Bits value as negative
+  DecompressFTPDeflate.  I have verified this on RaidenFTPD and ShareIt.
+
+  Note that there is an inconsistancy in the FTP Deflate drafts
+
+  http://community.roxen.com/developers/idocs/drafts/draft-preston-ftpext-deflat
+  e-02.html
+
+  and
+
+  http://community.roxen.com/developers/idocs/drafts/draft-preston-ftpext-deflat
+  e-00.html
+
+
+  Rev 1.14    6/14/2004 6:14:42 PM  JPMugaas
+  A fix from Bas.  RaidenFTPD 1455 (http://www.raidenftpd.com) was sending the
+
+  2 byte header, the compression methods flags
+
+  while ShareIt (www.noisette-software.com/products/windows/ShareIt/) was not
+  doing so.
+
+
+  Rev 1.13    2004.05.20 11:37:22 AM  czhower
+  IdStreamVCL
+
+
+  Rev 1.12    2/15/2004 6:56:44 AM  JPMugaas
+  GZip decompression should now work again.
+
+
+  Rev 1.11    2/15/2004 6:22:26 AM  JPMugaas
+  Fixed some parameter errors.
+
+
+  Rev 1.10    2/14/2004 9:59:48 PM  JPMugaas
+  Reworked the API.  There is now a separate API for the Inflate_ and
+  InflateInit2_ functions as well as separate functions for DeflateInit_ and
+  DeflateInit2_.  This was required for FTP.  The API also includes an optional
+  output stream for the servers.
+
+
+  Rev 1.9    2/12/2004 11:35:00 PM  JPMugaas
+  FTP Deflate preliminary support.  Work still needs to be done for upload and
+  downloading.
+
+
+  Rev 1.8    2/12/2004 11:11:24 AM  JPMugaas
+  Added methods for HTTP Compression and decompression using RFC 1950.  I have
+  verified these.
+
+
+  Rev 1.7    2004.02.03 5:45:48 PM  czhower
+  Name changes
+
+
+  Rev 1.6    10/25/2003 06:52:26 AM  JPMugaas
+  Updated for new API changes and tried to restore some functionality.
+
+
+  Rev 1.5    10/24/2003 05:04:54 PM  JPMugaas
+  SHould work as before.
+
+
+  Rev 1.4    2003.10.24 10:43:14 AM  czhower
+  TIdSTream to dos
+
+
+  Rev 1.3    10/7/2003 10:07:08 PM  GGrieve
+  Get HTTP compiling for DotNet
+
+
+  Rev 1.2    7/13/2003 10:57:30 PM  BGooijen
   Fixed GZip and Deflate decoding
-}
-{
-{   Rev 1.1    7/13/2003 11:30:56 AM  JPMugaas
-{ Stub methods for Deflate and inflate methods if needed.
-}
-{
-{   Rev 1.0    7/13/2003 11:08:38 AM  JPMugaas
-{ classes for ZLib compression.
+
+
+  Rev 1.1    7/13/2003 11:30:56 AM  JPMugaas
+  Stub methods for Deflate and inflate methods if needed.
+
+
+  Rev 1.0    7/13/2003 11:08:38 AM  JPMugaas
+  classes for ZLib compression.
 }
 unit IdZLibCompressorBase;
 
