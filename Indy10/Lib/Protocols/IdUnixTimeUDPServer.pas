b5cdaa255066cc02ac1660a79d1968e28a056fd4
Don Siders <indy@svn.indy.org>
2006-02-18 04:41:26 +0000

    Clean-up of old revision comments.

--- Indy10/Lib/Protocols/IdUnixTimeUDPServer.pas
+++ Indy10/Lib/Protocols/IdUnixTimeUDPServer.pas
@@ -16,30 +16,39 @@
   $Log$
 }
 {
-{   Rev 1.0    2/10/2005 2:26:38 PM  JPMugaas
-{ New UnixTime Service (port 519) components.
+  Rev 1.0    2/10/2005 2:26:38 PM  JPMugaas
+  New UnixTime Service (port 519) components.
 }
+
 unit IdUnixTimeUDPServer;
 
 interface
-uses IdAssignedNumbers, IdUDPServer, IdTimeUDPServer;
+
+uses
+  IdAssignedNumbers, IdUDPServer, IdTimeUDPServer;
+
 {
-This is based on a description at
-http://amo.net/AtomicTimeZone/help/ATZS_Protocols.htm#Unix
+  This is based on a description at
+  http://amo.net/AtomicTimeZone/help/ATZS_Protocols.htm#Unix
 
-UnixTime and UnixTime Protocol
-Unix is an operating system developed in 1969 by Ken Thompson. UnixTime counts "epochs" or seconds since the Year 1970. UnixTime recently hit it's billionth birthday.
+  UnixTime and UnixTime Protocol
+  Unix is an operating system developed in 1969 by Ken Thompson.
+  UnixTime counts "epochs" or seconds since the Year 1970. UnixTime
+  recently hit it's billionth birthday.
 
-Because Unix is widely used in many environments, UnixTime was developed into a loose simple time protocol in the late 80's and early 90's. No formal UnixTime protocol has ever been officially published as an internet protocol - until now.
+  Because Unix is widely used in many environments, UnixTime was developed
+  into a loose simple time protocol in the late 80's and early 90's. No
+  formal UnixTime protocol has ever been officially published as an internet
+  protocol - until now.
 
-UnixTime operates on the same UnixTime port - 519. Once a connection is
-requested on this port, exactly like in Time Protocol, the UnixTime value
-is sent back by either tcp/ip or udp/ip. When UDP/IP is used, a small packet
-of data must be received by the server in order to respond in the exact same
-fashion as Time Protocol. The UnixTime is then sent as an unsigned
-"unformatted" integer on the same port.
-
+  UnixTime operates on the same UnixTime port - 519. Once a connection is
+  requested on this port, exactly like in Time Protocol, the UnixTime value
+  is sent back by either tcp/ip or udp/ip. When UDP/IP is used, a small packet
+  of data must be received by the server in order to respond in the exact same
+  fashion as Time Protocol. The UnixTime is then sent as an unsigned
+  "unformatted" integer on the same port.
 }
+
 type
    TIdUnixTimeUDPServer = class(TIdCustomTimeUDPServer)
    protected
