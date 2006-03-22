b2e1bfcd82e8aa49a77a7b713d2c6b917df6c968
Remy Lebeau <indy@svn.indy.org>
2006-03-22 05:32:08 +0000

    Renamed TReadFileExclusiveStream to TIdReadFileExclusiveStream

--- Indy10/Lib/Protocols/IdMessageCoder.pas
+++ Indy10/Lib/Protocols/IdMessageCoder.pas
@@ -341,7 +341,7 @@ procedure TIdMessageEncoder.Encode(const AFilename: string; ADest: TIdStream);
 var
   LSrcStream: TIdStream;
 begin
-  LSrcStream := TReadFileExclusiveStream.Create(AFileName); try
+  LSrcStream := TIdReadFileExclusiveStream.Create(AFileName); try
     Encode(LSrcStream, ADest);
   finally Sys.FreeAndNil(LSrcStream); end;
 end;
