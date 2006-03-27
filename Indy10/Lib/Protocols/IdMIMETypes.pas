09f962bbafaf9b49366036b5f7fa5fb9b9a437eb
Remy Lebeau <indy@svn.indy.org>
2006-03-27 08:02:42 +0000

    Updated ReturnMIMEType() to use TextStartsWith(), and to remove Sys.LowerCase()

--- Indy10/Lib/Protocols/IdMIMETypes.pas
+++ Indy10/Lib/Protocols/IdMIMETypes.pas
@@ -21,12 +21,12 @@
 
   Rev 1.0    11/13/2002 07:57:36 AM  JPMugaas
 
-  2000-Mar-27 Pete Mee
-  - ReturnMIMETypes should return the most relevant MIME type+encoding pair
-    to the values passed - i.e., removes x- where it is unnecessary & modifies
-    the type if it is known to be incorrect.  (Stupid-)Example:
-    application/octet-stream + x-base64 should be application/octet-stream +
-    base64... Warning: this file is expected to grow to become mainly constants!!
+2000-Mar-27 Pete Mee
+ - ReturnMIMETypes should return the most relevant MIME type+encoding pair
+   to the values passed - i.e., removes x- where it is unnecessary & modifies
+   the type if it is known to be incorrect.  (Stupid-)Example:
+   application/octet-stream + x-base64 should be application/octet-stream +
+   base64... Warning: this file is expected to grow to become mainly constants!!
 }
 
 unit IdMIMETypes;
@@ -34,50 +34,49 @@ unit IdMIMETypes;
 interface
 
 const
-     MIMESplit = '/';    {Do not Localize}
-     MIMEXVal = 'x-';    {Do not Localize}
+  MIMESplit = '/';    {Do not Localize}
+  MIMEXVal = 'x-';    {Do not Localize}
 
-     MIMETypeApplication = 'application' + MIMESplit;    {Do not Localize}
-     MIMETypeAudio = 'audio' + MIMESplit;    {Do not Localize}
-     MIMETypeImage = 'image' + MIMESplit;    {Do not Localize}
-     MIMETypeMessage = 'message' + MIMESplit;    {Do not Localize}
-     MIMETypeMultipart = 'multipart' + MIMESplit;    {Do not Localize}
-     MIMETypeText = 'text' + MIMESplit;    {Do not Localize}
-     MIMETypeVideo = 'video' + MIMESplit;    {Do not Localize}
-     MaxMIMEType = 6;
+  MIMETypeApplication = 'application' + MIMESplit;    {Do not Localize}
+  MIMETypeAudio = 'audio' + MIMESplit;    {Do not Localize}
+  MIMETypeImage = 'image' + MIMESplit;    {Do not Localize}
+  MIMETypeMessage = 'message' + MIMESplit;    {Do not Localize}
+  MIMETypeMultipart = 'multipart' + MIMESplit;    {Do not Localize}
+  MIMETypeText = 'text' + MIMESplit;    {Do not Localize}
+  MIMETypeVideo = 'video' + MIMESplit;    {Do not Localize}
+  MaxMIMEType = 6;
 
-     // MIME Sub-Types
-     MIMESubOctetStream = 'octet-stream';    {Do not Localize}
-     MIMESubMacBinHex40 = 'mac-binhex40';    {Do not Localize}
-     MaxMIMESubTypes = 1;
+  // MIME Sub-Types
+  MIMESubOctetStream = 'octet-stream';    {Do not Localize}
+  MIMESubMacBinHex40 = 'mac-binhex40';    {Do not Localize}
+  MaxMIMESubTypes = 1;
 
-     // BinToASCII
-     MIMEEncBase64 = 'base64'; // Correct MIME type    {Do not Localize}
-     MIMEEncUUEncode = MIMEXVal + 'uu'; // A guess...    {Do not Localize}
-     MIMEEncXXEncode = MIMEXVal + 'xx'; // A guess...    {Do not Localize}
-     MaxMIMEBinToASCIIType = 2;
+  // BinToASCII
+  MIMEEncBase64 = 'base64'; // Correct MIME type    {Do not Localize}
+  MIMEEncUUEncode = MIMEXVal + 'uu'; // A guess...    {Do not Localize}
+  MIMEEncXXEncode = MIMEXVal + 'xx'; // A guess...    {Do not Localize}
+  MaxMIMEBinToASCIIType = 2;
 
-     // Message Digests - a MIME type probably doesn't exist for these...    {Do not Localize}
-     MIMEEncRSAMD2 = MIMEXVal + 'rsa-md2';    {Do not Localize}
-     MIMEEncRSAMD4 = MIMEXVal + 'rsa-md4';    {Do not Localize}
-     MIMEEncRSAMD5 = MIMEXVal + 'rsa-md5';    {Do not Localize}
-     MIMEEncNISTSHA = MIMEXVal + 'nist-sha';    {Do not Localize}
-     MaxMIMEMessageDigestType = 3;
+  // Message Digests - a MIME type probably doesn't exist for these...    {Do not Localize}
+  MIMEEncRSAMD2 = MIMEXVal + 'rsa-md2';    {Do not Localize}
+  MIMEEncRSAMD4 = MIMEXVal + 'rsa-md4';    {Do not Localize}
+  MIMEEncRSAMD5 = MIMEXVal + 'rsa-md5';    {Do not Localize}
+  MIMEEncNISTSHA = MIMEXVal + 'nist-sha';    {Do not Localize}
+  MaxMIMEMessageDigestType = 3;
 
-     // Compression Types
-     MIMEEncRLECompress = MIMEXVal + 'rle-compress'; // Probably doesn't exist    {Do not Localize}
-     MaxMIMECompressType = 0;
+  // Compression Types
+  MIMEEncRLECompress = MIMEXVal + 'rle-compress'; // Probably doesn't exist    {Do not Localize}
+  MaxMIMECompressType = 0;
 
-     MaxMIMEEncType = MaxMIMEBinToASCIIType + MaxMIMEMessageDigestType + 1 +
-       MaxMIMECompressType + 1;
+  MaxMIMEEncType = MaxMIMEBinToASCIIType + MaxMIMEMessageDigestType + 1 + MaxMIMECompressType + 1;
 
-     // Only put long, frequent full values in. Keep this list short, otherwise
-     // it'll be a nightmare & produce HUGE .exe files with LARGE useless    {Do not Localize}
-     // sections (the above is bad enough on it's own!).    {Do not Localize}
-     MIMEFullApplicationOctetStream = MIMETypeApplication + MIMESubOctetStream;
+  // Only put long, frequent full values in. Keep this list short, otherwise
+  // it'll be a nightmare & produce HUGE .exe files with LARGE useless    {Do not Localize}
+  // sections (the above is bad enough on it's own!).    {Do not Localize}
+  MIMEFullApplicationOctetStream = MIMETypeApplication + MIMESubOctetStream;
 
-     // Returns true if matched, false if not.  If true, vars may be altered.
-     function ReturnMIMEType(var MediaType, EncType : String) : Boolean;
+  // Returns true if matched, false if not.  If true, vars may be altered.
+  function ReturnMIMEType(var MediaType, EncType : String) : Boolean;
 
 var
    MIMEMediaType : array [0..MaxMIMEType] of String;
@@ -89,77 +88,71 @@ uses
   IdGlobalProtocols,
   IdSys;
 
-function ReturnMIMEType;
+function ReturnMIMEType(var MediaType, EncType : String) : Boolean;
 var
-   MType, SType, EType : String;
-   i : LongWord;
+  MType, SType, EType : String;
+  i : LongWord;
 begin
-     i := IndyPos(MIMESplit, MediaType);
-     MType := Copy(MediaType, 1, i);
-     SType := Copy(MediaType, i + 1, length(MediaType));
-     EType := EncType;
+  i := IndyPos(MIMESplit, MediaType);
+  MType := Copy(MediaType, 1, i);
+  SType := Copy(MediaType, i + 1, MaxInt);
+  EType := EncType;
 
-     i:=PosInStrArray(Sys.LowerCase(MType),MIMEMediaType);
-     case i of
-       0 : begin
-         // MIMETypeApplication - application/
-       end;
-       1 : begin
-         // MIMETypeAudio - audio/
-       end;
-       2 : begin
-         // MIMETypeImage - image/
-       end;
-       3 : begin
-         // MIMETypeMessage - message/
-       end;
-       4 : begin
-         // MIMETypeMultipart - multipart/
-       end;
-       5 : begin
-         // MIMETypeText - text/
-       end;
-       6 : begin
-         // MIMETypeVideo - video/
-       end;
-     else begin
-          if Sys.LowerCase(Copy(MType, 1, 2)) = MIMEXVal then begin
-             i:=PosInStrArray(Sys.LowerCase(Copy(MType, 3, length(MType))),MIMEMediaType);
-             case i of
-               0 : begin
-                 // MIMETypeApplication - application/
-                 MType := MIMETypeApplication;
-               end;
-               1 : begin
-                 // MIMETypeAudio - audio/
-                 MType := MIMETypeAudio;
-               end;
-               2 : begin
-                 // MIMETypeImage - image/
-                 MType := MIMETypeImage;
-               end;
-               3 : begin
-                 // MIMETypeMessage - message/
-                 MType := MIMETypeMessage;
-               end;
-               4 : begin
-                 // MIMETypeMultipart - multipart/
-                 MType := MIMETypeMultipart;
-               end;
-               5 : begin
-                 // MIMETypeText - text/
-                 MType := MIMETypeText;
-               end;
-               6 : begin
-                 // MIMETypeVideo - video/
-                 MType := MIMETypeVideo;
-               end;
-             end;
+  i := PosInStrArray(MType, MIMEMediaType, False);
+  case i of
+    0 : begin end; // MIMETypeApplication - application/
+    1 : begin end; // MIMETypeAudio - audio/
+    2 : begin end; // MIMETypeImage - image/
+    3 : begin end; // MIMETypeMessage - message/
+    4 : begin end; // MIMETypeMultipart - multipart/
+    5 : begin end; // MIMETypeText - text/
+    6 : begin end; // MIMETypeVideo - video/
+    else
+      begin
+        if TextStartsWith(MType, MIMEXVal) then begin
+          i := PosInStrArray(Copy(MType, 3, MaxInt), MIMEMediaType, False);
+          case i of
+            0 :
+              begin
+                // MIMETypeApplication - application/
+                MType := MIMETypeApplication;
+              end;
+            1 :
+              begin
+                // MIMETypeAudio - audio/
+                MType := MIMETypeAudio;
+              end;
+            2 :
+              begin
+                // MIMETypeImage - image/
+                MType := MIMETypeImage;
+              end;
+            3 :
+              begin
+                // MIMETypeMessage - message/
+                MType := MIMETypeMessage;
+              end;
+            4 :
+              begin
+                // MIMETypeMultipart - multipart/
+                MType := MIMETypeMultipart;
+              end;
+            5 :
+              begin
+                // MIMETypeText - text/
+                MType := MIMETypeText;
+              end;
+            6 :
+              begin
+                // MIMETypeVideo - video/
+                MType := MIMETypeVideo;
+              end;
           end;
-       end;
-     end;
+        end;
+      end;
+  end;
 
-     result := false;
+  Result := False;
 end;
 
 initialization
