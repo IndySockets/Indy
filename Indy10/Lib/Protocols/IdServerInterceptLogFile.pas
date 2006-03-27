699d1bc37ed4387b1930574a05f06d020bfd12c6
Remy Lebeau <indy@svn.indy.org>
2006-03-27 06:38:00 +0000

    Added 'const to various string parameters

--- Indy10/Lib/Protocols/IdServerInterceptLogFile.pas
+++ Indy10/Lib/Protocols/IdServerInterceptLogFile.pas
@@ -26,13 +26,13 @@
   Rev 1.3    6/29/04 1:20:14 PM  RLebeau
   Updated DoLogWriteString() to call WriteStringToStream() instead
 
-  Rev 1.2    10/19/2003 5:57:22 PM  DSiders
+    Rev 1.2    10/19/2003 5:57:22 PM  DSiders
   Added localization comments.
 
   Rev 1.1    2003.10.17 8:20:42 PM  czhower
   Removed const
 
-  Rev 1.0    3/22/2003 10:59:22 PM  BGooijen
+    Rev 1.0    3/22/2003 10:59:22 PM  BGooijen
   Initial check in.
   ServerIntercept to ease debugging, data/status are logged to a file
 }
@@ -53,10 +53,10 @@ type
     FFilename:string;
   public
     procedure Init; override;
-    destructor Destroy;override;
-    procedure DoLogWriteString(AText: string);override;
+    destructor Destroy; override;
+    procedure DoLogWriteString(const AText: string); override;
   published
-    property Filename:string read FFilename write FFilename;
+    property Filename: string read FFilename write FFilename;
   end;
 
 implementation
@@ -69,7 +69,7 @@ uses
 destructor TIdServerInterceptLogFile.Destroy;
 begin
   Sys.FreeAndNil(FFileStream);
-  inherited;
+  inherited Destroy;
 end;
 
 procedure TIdServerInterceptLogFile.Init;
@@ -79,11 +79,11 @@ begin
     if FFilename = '' then begin
       FFilename := Sys.ChangeFileExt(ParamStr(0), '.log'); {do not localize}  //BGO: TODO: Do we keep this, or maybe raise an exception?
     end;
-    FFileStream := TIdAppendFileStream.Create(FFileName);
+    FFileStream := TIdAppendFileStream.Create(Filename);
   end;
 end;
 
-procedure TIdServerInterceptLogFile.DoLogWriteString(AText: string);
+procedure TIdServerInterceptLogFile.DoLogWriteString(const AText: string);
 begin
   WriteStringToStream(FFileStream, AText);
 end;
