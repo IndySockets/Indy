fe91e48bf186e742cfc83886dca4d20f797c8265
Remy Lebeau <indy@svn.indy.org>
2006-03-27 06:34:44 +0000

    Updated GenerateOTP() to use new TIdOTPCalculator.GenerateSixWordKey() method.

--- Indy10/Lib/Protocols/IdSASLOTP.pas
+++ Indy10/Lib/Protocols/IdSASLOTP.pas
@@ -22,7 +22,7 @@
   Rev 1.3    1/21/2004 4:03:16 PM  JPMugaas
   InitComponent
 
-  Rev 1.2    10/19/2003 5:57:18 PM  DSiders
+    Rev 1.2    10/19/2003 5:57:18 PM  DSiders
   Added localization comments.
 
   Rev 1.1    5/10/2003 10:10:44 PM  JPMugaas
@@ -33,7 +33,7 @@
   mechanism.
 }
 
-{ This is based on RFC2444 }
+{This is based on RFC2444}
 
 unit IdSASLOTP;
 
@@ -47,19 +47,13 @@ uses
 type
   TIdSASLOTP = class(TIdSASLUserPass)
   protected
-    function GenerateOTP(const AResponse:string; const APassword:string):string;
+    function GenerateOTP(const AResponse, APassword: String): String;
     procedure InitComponent; override;
   public
     class function ServiceName: TIdSASLServiceName; override;
-
-    function StartAuthenticate(const AChallenge:string) : String; override;
-    function ContinueAuthenticate(const ALastResponse: String): String;
-      override;
-
+    function StartAuthenticate(const AChallenge: String): String; override;
+    function ContinueAuthenticate(const ALastResponse: String): String; override;
   end;
-
-  EIdOTPSASLException = class(EIdException);
-  EIdOTPSASLUnknownOTPMethodException = class(EIdOTPSASLException);
 
 implementation
 
@@ -68,15 +62,14 @@ uses
 
 { TIdSASLOTP }
 
-function TIdSASLOTP.ContinueAuthenticate(
-  const ALastResponse: String): String;
+function TIdSASLOTP.ContinueAuthenticate(const ALastResponse: String): String;
 begin
   Result := GenerateOTP(ALastResponse, GetPassword);
 end;
 
 procedure TIdSASLOTP.InitComponent;
 begin
-  inherited;
+  inherited InitComponent;
   FSecurityLevel := 1000;
 end;
 
@@ -90,42 +83,14 @@ begin
   Result := GetUsername;
 end;
 
-function TIdSASLOTP.GenerateOTP(const AResponse:string; const APassword:string):string;
-var LChallenge:string;
-    LChallengeStartPos:integer;
-    LMethod:string;
-    LSeed:string;
-    LCount:integer;
+function TIdSASLOTP.GenerateOTP(const AResponse, APassword: String): String;
+var
+  LKey: String;
 begin
-  LChallengeStartPos:=pos('otp-', AResponse); {do not localize}
-  if LChallengeStartPos>0 then begin
-    inc(LChallengeStartPos, 4); // to remove "otp-"
-    LChallenge:=copy(AResponse, LChallengeStartPos, $FFFF);
-    LMethod:=Fetch(LChallenge);
-    LCount:=Sys.StrToInt(Fetch(LChallenge));
-    LSeed:=Fetch(LChallenge);
-    if LMethod='md5' then {do not localize} // methods are case sensitive
-    begin
-      Result := 'word:' + TIdOTPCalculator.ToSixWordFormat(TIdOTPCalculator.GenerateKeyMD5(lseed, APassword, LCount)) {do not localize}
-    end
-    else
-    begin
-      if LMethod = 'md4' then {do not localize}
-      begin
-        Result := 'word:' + TIdOTPCalculator.ToSixWordFormat(TIdOTPCalculator.GenerateKeyMD4(lseed, APassword, LCount)) {do not localize}
-      end
-      else
-      begin
-        if LMethod = 'sha1' then  {do not localize}
-        begin
-           Result := 'word:' + TIdOTPCalculator.ToSixWordFormat(TIdOTPCalculator.GenerateKeySHA1(lseed, APassword, LCount)) {do not localize}
-        end
-        else
-        begin
-          raise EIdOTPSASLUnknownOTPMethodException.Create('Unknown OTP method'); //rs  {do not localize}
-        end;
-      end;
-    end;
+  if TIdOTPCalculator.GenerateSixWordKey(AResponse, APassword, LKey) then begin
+    Result := 'word:' + LKey; {do not localize}
+  end else begin
+    Result := '';
   end;
 end;
 
