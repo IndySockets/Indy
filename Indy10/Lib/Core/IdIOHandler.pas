b6ef0531669012a97ec3abd98b2a307d096cd964
Remy Lebeau <indy@svn.indy.org>
2006-05-23 19:16:51 +0000

    Changed ReadStream() to handle disconnect exceptions better so that pending data is removed from the InputBuffer properly

--- Indy10/Lib/Core/IdIOHandler.pas
+++ Indy10/Lib/Core/IdIOHandler.pas
@@ -1276,7 +1276,7 @@ const
 begin
   Assert(AStream<>nil);
 
-  if (AByteCount = cSizeUnknown) and (AReadUntilDisconnect = False) then begin
+  if (AByteCount = cSizeUnknown) and (not AReadUntilDisconnect) then begin
     // Read size from connection
     if LargeStream then begin
       AByteCount := ReadInt64;
@@ -1308,7 +1308,12 @@ begin
       Dec(LWorkCount, i);
     end;
 
-    while Connected and (LWorkCount > 0) do begin
+    // RLebeau - don't call Connected() here!  ReadBytes() already
+    // does that internally. Calling Connected() here can cause an
+    // EIdConnClosedGracefully exception that breaks the loop
+    // prematurely and thus leave unread bytes in the InputBuffer.
+    // Let the loop catch the exception before exiting...
+    while {Connected and} (LWorkCount > 0) do begin
       i := Min(LWorkCount, RecvBufferSize);
       //TODO: Improve this - dont like the use of the exception handler
       //DONE -oAPR: Dont use a string, use a memory buffer or better yet the buffer itself.
@@ -1325,7 +1330,7 @@ begin
             i := Min(i, FInputBuffer.Size);
             FInputBuffer.ExtractToBytes(LBuf, i);
             if (E is EIdConnClosedGracefully) and AReadUntilDisconnect then begin
-              break;
+              Break;
             end else begin
               raise;
             end;
@@ -1333,7 +1338,7 @@ begin
         end;
       finally
         if i > 0 then begin
-          TIdStreamHelper.Write(AStream,LBuf,i);
+          TIdStreamHelper.Write(AStream, LBuf, i);
           Dec(LWorkCount, i);
         end;
       end;
