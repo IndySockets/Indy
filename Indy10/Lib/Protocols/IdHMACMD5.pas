c80f4d2b11d461fb38957b2552b138018fb2169d
Remy Lebeau <indy@svn.indy.org>
2006-09-06 03:01:29 +0000

    Updated InternalHashValue() to reflect changes in TIdHashMessageDigest5.HashValue() method

--- Indy10/Lib/Protocols/IdHMACMD5.pas
+++ Indy10/Lib/Protocols/IdHMACMD5.pas
@@ -52,24 +52,12 @@ end;
 function TIdHMACMD5.InternalHashValue(const ABuffer: TIdBytes): TIdBytes;
 var
   TempStream: TIdMemoryStream;
-  TempResult: T4x4LongWordRecord;
-  TempBuff: TIdBytes;
 begin
   TempStream := TIdMemoryStream.Create;
   try
     WriteTIdBytesToStream(TempStream, ABuffer);
     TempStream.Position := 0;
-    TempResult := FHashMD5.HashValue(TempStream);
-    SetLength(TempBuff, 4);
-    SetLength(Result, FHashSize);
-    TempBuff := ToBytes(TempResult[0]);
-    CopyTIdBytes(TempBuff, 0, Result, 0, 4);
-    TempBuff := ToBytes(TempResult[1]);
-    CopyTIdBytes(TempBuff, 0, Result, 4, 4);
-    TempBuff := ToBytes(TempResult[2]);
-    CopyTIdBytes(TempBuff, 0, Result, 8, 4);
-    TempBuff := ToBytes(TempResult[3]);
-    CopyTIdBytes(TempBuff, 0, Result, 12, 4);
+    Result := FHashMD5.HashValue(TempStream);
   finally
     Sys.FreeAndNil(TempStream);
   end;
