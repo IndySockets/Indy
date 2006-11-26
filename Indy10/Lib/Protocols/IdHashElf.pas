8cef8c59233de194d7b7aeb7079f9857d45e5923
Remy Lebeau <indy@svn.indy.org>
2006-11-26 07:52:26 +0000

    Moved GetHashBytes() to TIdHash32

--- Indy10/Lib/Protocols/IdHashElf.pas
+++ Indy10/Lib/Protocols/IdHashElf.pas
@@ -27,12 +27,10 @@ unit IdHashElf;
 interface
 
 uses
-  IdGlobal, IdHash, IdObjs;
+  IdHash;
 
 type
   TIdHashElf = class(TIdHash32)
-  protected
-    function GetHashBytes(AStream: TIdStream; ASize: Int64): TIdBytes; override;
   public
     procedure HashStart(var VRunningHash : LongWord); override;
     procedure HashByte(var VRunningHash : LongWord; const AByte : Byte); override;
@@ -42,40 +40,9 @@ implementation
 
 { TIdHashElf }
 
-function TIdHashElf.GetHashBytes(AStream: TIdStream; ASize: Int64): TIdBytes;
-const
-  cBufSize = 1024; // Keep it small for dotNET
-var
-  I: Integer;
-  LBuffer: TIdBytes;
-  LSize: Integer;
-  LHash, LTemp: LongWord;
+procedure TIdHashElf.HashStart(var VRunningHash: LongWord);
 begin
-  Result := nil;
-  LHash := 0;
-
-  SetLength(LBuffer, cBufSize);
-
-  while ASize > 0 do
-  begin
-    LSize := ReadTIdBytesFromStream(AStream, LBuffer, cBufSize);
-    if LSize < 1 then begin
-      Break; // TODO: throw a stream read exception instead?
-    end;
-    for i := 0 to LSize-1 do
-    begin
-      LHash := (LHash shl 4) + LBuffer[i];
-      LTemp := LHash and $F0000000;
-      if LTemp <> 0 then begin
-        LHash := LHash xor (LTemp shr 24);
-      end;
-      LHash := LHash and (not LTemp);
-    end;
-    Dec(ASize, LSize);
-  end;
-
-  SetLength(Result, SizeOf(LongWord));
-  CopyTIdLongWord(LHash, Result, 0);
+  VRunningHash := 0;
 end;
 
 procedure TIdHashElf.HashByte(var VRunningHash: LongWord; const AByte: Byte);
@@ -88,11 +55,6 @@ begin
     VRunningHash := VRunningHash xor (LTemp shr 24);
   end;
   VRunningHash := VRunningHash and (not LTemp);
-end;
-
-procedure TIdHashElf.HashStart(var VRunningHash: LongWord);
-begin
-  VRunningHash := 0;
 end;
 
 end.
