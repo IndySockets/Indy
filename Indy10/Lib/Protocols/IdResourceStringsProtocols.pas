f3fac3679ba80c0a1ae19742853e31c849c910f9
Remy Lebeau <indy@svn.indy.org>
2007-01-03 03:44:42 +0000

    Added RSSMTPUserNotLocalFwdAddr

--- Indy10/Lib/Protocols/IdResourceStringsProtocols.pas
+++ Indy10/Lib/Protocols/IdResourceStringsProtocols.pas
@@ -382,7 +382,6 @@ resourcestring
   RSQueryPackReceivedTooSmall = 'Received Packet is too small. %d';
   RSQueryUnknownError = 'Unknown Error %d, Id %d';
   RSQueryInvalidIpV6 = 'Invalid IP V6 Address. %s';
-
   RSQueryMustProvideSOARecord = 'You have to provide a TIdRR_SOA object with Serial number and Name to progress IXFR. %d';
  
   { LPD Client Logging event strings }
@@ -615,6 +614,7 @@ resourcestring
   RSSMTPMailboxUnavailable = 'Requested action not taken: mailbox unavailable';
   RSSMTPUserNotLocal = 'User %s not local; please try <%s>';
   RSSMTPUserNotLocalNoAddr = 'User %s not local; no forwarding address';
+  RSSMTPUserNotLocalFwdAddr = 'User %s not local; will forward to <%s>';
   RSSMTPTooManyRecipients = 'Too Many recipients.';
   RSSMTPAccountDisabled = '%s Account Disabled';
 
