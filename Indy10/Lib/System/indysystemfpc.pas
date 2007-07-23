caa72e01f982055163d53c554cbbf8503d9c86e6
J. Peter Mugaas <indy@svn.indy.org>
2007-07-23 20:17:41 +0000

    (empty log message)

---
+++ Indy10/Lib/System/indysystemfpc.pas
@@ -0,0 +1,21 @@
+unit indysystemfpc;
+interface
+
+uses
+  IdAntiFreezeBase,
+  IdBaseComponent,
+  IdComponent,
+  IdException,
+  IdGlobal,
+  IdResourceStrings,
+  IdStack,
+  IdStackConsts,
+  IdStream,
+  IdStreamVCL,
+  IdStruct,
+  IdSys,
+  IdSysBase;
+
+implementation
+
+end.
