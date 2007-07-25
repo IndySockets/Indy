a5fcd73092bf133def5f501aaa3f65fd94ad8e64
J. Peter Mugaas <indy@svn.indy.org>
2007-07-25 13:40:18 +0000

    (empty log message)

---
+++ Indy10/Lib/debian/indy-fpc-default.ex
@@ -0,0 +1,10 @@
+# Defaults for indy-fpc initscript
+# sourced by /etc/init.d/indy-fpc
+# installed at /etc/default/indy-fpc by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
