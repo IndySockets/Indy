96559ed7c0dfe1eedcc216ca59ca6c36196e400c
Remy Lebeau <indy@svn.indy.org>
2007-07-26 01:58:02 +0000

    Code cleanup

--- Indy10/Lib/Protocols/IdFTPListParseChameleonNewt.pas
+++ Indy10/Lib/Protocols/IdFTPListParseChameleonNewt.pas
@@ -23,29 +23,34 @@
 unit IdFTPListParseChameleonNewt;
 
 interface
+
 {$i IdCompilerDefines.inc}
+
 uses
   Classes,
   IdFTPList, IdFTPListParseBase,IdFTPListTypes;
 
 type
   TIdChameleonNewtFTPListItem = class(TIdDOSBaseFTPListItem);
+
   TIdFTPLPChameleonNewt = class(TIdFTPLPBaseDOS)
   protected
     class function MakeNewItem(AOwner : TIdFTPListItems)  : TIdFTPListItem; override;
-    class function ParseLine(const AItem : TIdFTPListItem; const APath : String=''): Boolean; override;
+    class function ParseLine(const AItem : TIdFTPListItem; const APath : String = ''): Boolean; override;
   public
     class function GetIdent : String; override;
-    class function CheckListing(AListing : TStrings; const ASysDescript : String =''; const ADetails : Boolean = True): boolean; override;
+    class function CheckListing(AListing : TStrings; const ASysDescript : String = ''; const ADetails : Boolean = True): Boolean; override;
   end;
 
 implementation
-uses IdFTPCommon, IdGlobal, IdGlobalProtocols, SysUtils;
+
+uses
+  IdFTPCommon, IdGlobal, IdGlobalProtocols, SysUtils;
 
 { TIdFTPLPChameleonNewt }
 
 class function TIdFTPLPChameleonNewt.CheckListing(AListing: TStrings;
-  const ASysDescript: String; const ADetails: Boolean): boolean;
+  const ASysDescript: String; const ADetails: Boolean): Boolean;
 {Look for something like this:
 
 .               <DIR>      Nov 16 1994 17:16
@@ -57,7 +62,8 @@ README.TXT       1045      May 10 1995 11:01
 WPKIT1.EXE     960338      Jun 21 1995 17:01   R
 CMT.CSV             0      Jul 06 1995 14:56   RHA
 }
-var i : Integer;
+var
+  i : Integer;
   LBuf, LBuf2 : String;
   LInt : Integer;
 begin
@@ -71,58 +77,46 @@ begin
     LBuf := TrimLeft(LBuf);
     //<DIR> or file size
     LBuf2 := Fetch(LBuf);
-    Result := (LBuf2='<DIR>') or IsNumeric(LBuf2);   {Do not localize}
-    if not result then
-    begin
+    Result := (LBuf2 = '<DIR>') or IsNumeric(LBuf2);   {Do not localize}
+    if not Result then begin
       Exit;
     end;
     LBuf := TrimLeft(LBuf);
     //month
     LBuf2 := Fetch(LBuf);
-    Result := StrToMonth(LBuf2)>0;
-    if not result then
-    begin
+    Result := StrToMonth(LBuf2) > 0;
+    if not Result then begin
       Exit;
     end;
     //day
     LBuf := TrimLeft(LBuf);
-    LInt := IndyStrToInt64(Fetch(LBuf),0);
-    Result := (LInt>0) and (LInt<32);
-    if not result then
-    begin
+    LInt := IndyStrToInt64(Fetch(LBuf), 0);
+    Result := (LInt > 0) and (LInt < 32);
+    if not Result then begin
       Exit;
     end;
     //year
     LBuf := TrimLeft(LBuf);
     Result := IsNumeric(Fetch(LBuf));
-    if not result then
-    begin
+    if not Result then begin
       Exit;
     end;
     //time
     LBuf := TrimLeft(LBuf);
     LBuf2 := Fetch(LBuf);
-    Result := IsHHMMSS(LBuf2,':');
-    if not result then
-    begin
+    Result := IsHHMMSS(LBuf2, ':');
+    if not Result then begin
       Exit;
     end;
     //attributes
     repeat
-
       LBuf := TrimLeft(LBuf);
-      if LBuf='' then
-      begin
-        break;
+      if LBuf = '' then begin
+        Break;
       end;
       LBuf2 := Fetch(LBuf);
-
-      result := IsValidAttr(LBuf2);
-      if not result then
-      begin
-        Exit;
-      end;
-    until False;
+      Result := IsValidAttr(LBuf2);
+    until not Result;
   end;
 end;
 
@@ -131,15 +125,15 @@ begin
   Result := 'NetManage Chameleon/Newt';  {Do not localize}
 end;
 
-class function TIdFTPLPChameleonNewt.MakeNewItem(
-  AOwner: TIdFTPListItems): TIdFTPListItem;
+class function TIdFTPLPChameleonNewt.MakeNewItem(AOwner: TIdFTPListItems): TIdFTPListItem;
 begin
   Result := TIdChameleonNewtFTPListItem.Create(AOwner);
 end;
 
 class function TIdFTPLPChameleonNewt.ParseLine(const AItem: TIdFTPListItem;
   const APath: String): Boolean;
-var LI : TIdChameleonNewtFTPListItem;
+var
+  LI : TIdChameleonNewtFTPListItem;
   LBuf, LBuf2 : String;
   LDay, LMonth, LYear : Integer;
 begin
@@ -155,69 +149,57 @@ begin
   begin
     LI.ItemType := ditDirectory;
     LI.SizeAvail := False;
-  end
-  else
+  end else
   begin
     LI.ItemType := ditFile;
     Result :=  IsNumeric(LBuf2);
-    if not result then
-    begin
+    if not Result then begin
       Exit;
     end;
-    LI.Size := IndyStrToInt64(LBuf2,0);
+    LI.Size := IndyStrToInt64(LBuf2, 0);
   end;
-
   //month
+  LBuf := TrimLeft(LBuf);
+  LBuf2 := Fetch(LBuf);
+  LMonth := StrToMonth(LBuf2);
+  Result := LMonth > 0;
+  if not Result then begin
+    Exit;
+  end;
+  //day
+  LBuf := TrimLeft(LBuf);
+  LBuf2 := Fetch(LBuf);
+  LDay := IndyStrToInt64(LBuf2, 0);
+  Result := (LDay > 0) and (LDay < 32);
+  if not Result then begin
+    Exit;
+  end;
+  //year
+  LBuf := TrimLeft(LBuf);
+  LBuf2 := Fetch(LBuf);
+  Result := IsNumeric(LBuf2);
+  if not Result then begin
+    Exit;
+  end;
+  LYear := Y2Year(IndyStrToInt(LBuf2, 0));
+  LI.ModifiedDate := EncodeDate(LYear, LMonth, LDay);
+  //time
+  LBuf := TrimLeft(LBuf);
+  LBuf2 := Fetch(LBuf);
+  Result := IsHHMMSS(LBuf2, ':');
+  if not Result then begin
+    Exit;
+  end;
+  LI.ModifiedDate := LI.ModifiedDate + TimeHHMMSS(LBuf2);
+  //attributes
+  repeat
     LBuf := TrimLeft(LBuf);
-    LBuf2 := Fetch(LBuf);
-    LMonth := StrToMonth(LBuf2);
-    Result := LMonth>0;
-    if not result then
-    begin
-      Exit;
+    if LBuf = '' then begin
+      Break;
     end;
-    //day
-    LBuf := TrimLeft(LBuf);
     LBuf2 := Fetch(LBuf);
-    LDay := IndyStrToInt64(LBuf2,0);
-    Result := (LDay>0) and (LDay<32);
-    if not result then
-    begin
-      Exit;
-    end;
-    //year
-    LBuf := TrimLeft(LBuf);
-    LBuf2 := Fetch(LBuf);
-    Result := IsNumeric(LBuf2);
-    if not result then
-    begin
-      Exit;
-    end;
-    LYear := Y2Year( IndyStrToInt(LBuf2,0));
-    LI.ModifiedDate := EncodeDate(LYear,LMonth,LDay);
-    //time
-    LBuf := TrimLeft(LBuf);
-    LBuf2 := Fetch(LBuf);
-    Result := IsHHMMSS(LBuf2,':');
-    if not result then
-    begin
-      Exit;
-    end;
-    LI.ModifiedDate := LI.ModifiedDate + TimeHHMMSS(LBuf2);
-    //attributes
-    repeat
-      if LBuf='' then
-      begin
-        break;
-      end;
-      LBuf := TrimLeft(LBuf);
-      LBuf2 := Fetch(LBuf);
-      result := LI.FAttributes.AddAttribute(LBuf2);
-      if not result then
-      begin
-        Exit;
-      end;
-    until False;
+    Result := LI.FAttributes.AddAttribute(LBuf2);
+  until not Result;
 end;
 
 initialization
