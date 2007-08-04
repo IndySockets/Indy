b4846896c202d7821eb1b2634eda1de95ed0e552
Remy Lebeau <indy@svn.indy.org>
2007-08-04 10:48:50 +0000

    Updated for Win64/CE

--- Indy10/Lib/Protocols/IdZLibConst.pas
+++ Indy10/Lib/Protocols/IdZLibConst.pas
@@ -10,22 +10,27 @@
 unit IdZLibConst;
 
 interface
+
 {$i IdCompilerDefines.inc}
+
 {$IFNDEF FPC}
-{$IFDEF WIN32}
-  {$define STATICLOAD}
-{$ENDIF}
+  {$IFDEF WIN32_OR_WIN64_OR_WINCE}
+  {$DEFINE STATICLOAD}
+  {$ENDIF}
 {$ENDIF}
 {$IFNDEF STATICLOAD}
 uses
   IdException;
 {$ENDIF}
+
 resourcestring
  sTargetBufferTooSmall = 'ZLib error: target buffer may be too small';
  sInvalidStreamOp = 'Invalid stream operation';
+
 {$IFNDEF STATICLOAD} 
 RSZLibCallError = 'Error on call to ZLib library function %s';
 {$ENDIF}
+
 implementation
 
 end.
