9af24583a49b60e7140efdbb8bab7881d533238b
Remy Lebeau <indy@svn.indy.org>
2007-08-04 01:01:59 +0000

    Updated commands to send their output to nul

--- Indy10/Lib/Clean.bat
+++ Indy10/Lib/Clean.bat
@@ -1,15 +1,15 @@
 @echo off
-if exist %1*.dcu del %1*.dcu
-if exist %1*.dpl del %1*.dpl
-if exist %1*.bpl del %1*.bpl
-if exist %1*.bpi del %1*.bpi
-if exist %1*.lsp del %1*.lsp
-if exist %1*.dcp del %1*.dcp
-if exist %1*.dpc del %1*.dpc
-if exist %1*.bak del %1*.bak
-if exist %1Id*.obj del %1*.obj
-if exist %1Id*.hpp del %1*.hpp
-if exist %1*.lib del %1*.lib
-if exist %1*.~*  del %1*.~*
-if exist %1setenv.bat del %1setenv.bat
+if exist %1*.dcu del %1*.dcu >nul
+if exist %1*.dpl del %1*.dpl >nul
+if exist %1*.bpl del %1*.bpl >nul
+if exist %1*.bpi del %1*.bpi >nul
+if exist %1*.lsp del %1*.lsp >nul
+if exist %1*.dcp del %1*.dcp >nul
+if exist %1*.dpc del %1*.dpc >nul
+if exist %1*.bak del %1*.bak >nul
+if exist %1Id*.obj del %1*.obj >nul
+if exist %1Id*.hpp del %1*.hpp >nul
+if exist %1*.lib del %1*.lib >nul
+if exist %1*.~*  del %1*.~* >nul
+if exist %1setenv.bat del %1setenv.bat >nul
 
