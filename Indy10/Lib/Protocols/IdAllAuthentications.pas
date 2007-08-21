e43bdcf4b8d159f11ff528428f063507ec87971f
Remy Lebeau <indy@svn.indy.org>
2007-08-21 23:24:53 +0000

    (empty log message)

---
+++ Indy10/Lib/Protocols/IdAllAuthentications.pas
@@ -0,0 +1,24 @@
+unit IdAllAuthentications;
+
+interface
+
+{$i IdCompilerDefines.inc}
+
+{
+Note that this unit is simply for listing ALL Authentications in Indy.
+The user could then add this unit to a uses clause in their program and
+have all Authentications linked into their program.
+
+ABSOLUTELY NO CODE is permitted in this unit.
+
+}
+
+implementation
+
+uses
+  IdAuthenticationDigest,
+  IdAuthenticationNTLM;
+
+{dee-duh-de-duh, that's all folks.}
+
+end.
