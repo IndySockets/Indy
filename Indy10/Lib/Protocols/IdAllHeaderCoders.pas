724fd9381ea99ef02c8498adf5de38f8be53b6c0
Remy Lebeau <indy@svn.indy.org>
2007-08-21 21:27:52 +0000

    Moved used units into the implementation section

--- Indy10/Lib/Protocols/IdAllHeaderCoders.pas
+++ Indy10/Lib/Protocols/IdAllHeaderCoders.pas
@@ -5,7 +5,7 @@ interface
 {$i IdCompilerDefines.inc}
 
 {
-Note that is unit is simply for listing ALL Header coders in Indy.
+Note that this unit is simply for listing ALL Header coders in Indy.
 The user could then add this unit to a uses clause in their program and
 have all Header coders linked into their program.
 
@@ -13,13 +13,13 @@ ABSOLUTELY NO CODE is permitted in this unit.
 
 }
 
+implementation
+
 uses
   IdHeaderCoderPlain,
   IdHeaderCoder2022JP,
   {$IFDEF DOTNET}IdHeaderCoderDotNet,{$ENDIF}
   IdHeaderCoderUTF8;
-
-implementation
 
 {dee-duh-de-duh, that's all folks.}
 
