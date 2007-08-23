d9ec02130431559ee42914b3edc1999459f58ac6
J. Peter Mugaas <indy@svn.indy.org>
2007-08-23 21:42:46 +0000

    Return properties from SslStream and added an event for knowing when SSL negitation is complete.  This way you can log SSL information.

--- Indy10/Lib/Protocols/IdSSLDotNET.pas
+++ Indy10/Lib/Protocols/IdSSLDotNET.pas
@@ -2,6 +2,7 @@ unit IdSSLDotNET;
 
 interface
 uses
+  Classes,
   IdException,
   IdGlobal,
   IdStackDotNet,
@@ -19,13 +20,14 @@ type
     AsslPolicyErrors : SslPolicyErrors; var VValid : Boolean) of object;
   TIdSSLIOHandlerSocketNET = class(TIdSSLIOHandlerSocketBase)
   protected
+    FEnabledProtocols: System.Security.Authentication.SslProtocols;
     FOnValidatePeerCertificate : TOnValidatePeerCertificate;
     FSSL : SslStream;
     FSocketStream: Stream;
     FActiveStream: Stream;
     FServerCertificate : X509Certificate;
     FClientCertificates : X509CertificateCollection;
-    FEnabledProtocols : System.Security.Authentication.SslProtocols;
+    FOnSSLHandshakeDone : TNotifyEvent;
     procedure OpenEncodedConnection; virtual;
 
     //Ssl certificate validation callback
@@ -41,19 +43,47 @@ type
     procedure InitComponent; override;
 
     procedure ConnectClient; override;
+    //
+    function GetCipherAlgorithm: CipherAlgorithmType;
+    function GetCipherStrength: Integer;
+    function GetHashAlgorithm: HashAlgorithmType;
+    function GetHashStrength: Integer;
+    function GetIsEncrypted: Boolean;
+    function GetIsSigned: Boolean;
+    function GetKeyExchangeAlgorithm: ExchangeAlgorithmType;
+    function GetKeyExchangeStrength: Integer;
+    function GetRemoteCertificate: X509Certificate;    
+  published
+  published
   public
     procedure Close; override;
     procedure Open; override;
     procedure StartSSL; override;
     function Clone : TIdSSLIOHandlerSocketBase; override;
+    function GetSslProtocol: SslProtocols;
+
     property EnabledProtocols : System.Security.Authentication.SslProtocols read FEnabledProtocols write FEnabledProtocols;
     property ServerCertificate : X509Certificate read FServerCertificate write FServerCertificate;
     property ClientCertificates : X509CertificateCollection read FClientCertificates write FClientCertificates;
+
+    property OnSSLHandshakeDone : TNotifyEvent read FOnSSLHandshakeDone write FOnSSLHandshakeDone;
+
+    property CipherAlgorithm : CipherAlgorithmType read GetCipherAlgorithm;
+    property CipherStrength : Integer read GetCipherStrength;
+    property HashAlgorithm : HashAlgorithmType read GetHashAlgorithm;
+    property HashStrength : Integer read GetHashStrength;
+    property IsEncrypted : Boolean read GetIsEncrypted;
+    property IsSigned : Boolean read GetIsSigned;
+    property KeyExchangeAlgorithm : ExchangeAlgorithmType read GetKeyExchangeAlgorithm;
+    property KeyExchangeStrength : Integer read GetKeyExchangeStrength;
+    property RemoteCertificate : X509Certificate read GetRemoteCertificate;
+    property SslProtocol : SslProtocols read GetSslProtocol;
   end;
 
   EIdSSLNetException = class(EIdException);
   EIdSSLCertRequiredForSvr = class(EIdSSLNetException);
-  
+  EIdSSLNotAuthenticated = class(EIdSSLNetException);
+
 implementation
 uses
   IdStack, SysUtils;
@@ -103,6 +133,126 @@ begin
   StartSSL;
 end;
 
+function TIdSSLIOHandlerSocketNET.GetCipherAlgorithm: CipherAlgorithmType;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.CipherAlgorithm;
+  end
+  else
+  begin
+    Result := System.Security.Authentication.CipherAlgorithmType.None;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetCipherStrength: Integer;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.CipherStrength;
+  end
+  else
+  begin
+    Result := 0;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetHashAlgorithm: HashAlgorithmType;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.HashAlgorithm;
+  end
+  else
+  begin
+    Result := HashAlgorithmType.None;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetHashStrength: Integer;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.HashStrength;
+  end
+  else
+  begin
+    Result := 0;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetIsEncrypted: Boolean;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.IsEncrypted;
+  end
+  else
+  begin
+    Result := False;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetIsSigned: Boolean;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.IsSigned;
+  end
+  else
+  begin
+    Result := False;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetKeyExchangeAlgorithm: ExchangeAlgorithmType;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.KeyExchangeAlgorithm;
+  end
+  else
+  begin
+    Result := ExchangeAlgorithmType.None;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetKeyExchangeStrength: Integer;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.KeyExchangeStrength;
+  end
+  else
+  begin
+    Result := 0;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetRemoteCertificate: X509Certificate;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.RemoteCertificate;
+  end
+  else
+  begin
+    Result := nil;
+  end;
+end;
+
+function TIdSSLIOHandlerSocketNET.GetSslProtocol: SslProtocols;
+begin
+  if Assigned(FSSL) then
+  begin
+    Result := FSSL.SslProtocol;
+  end
+  else
+  begin
+    Result := SslProtocols.None;
+  end;
+end;
+
 procedure TIdSSLIOHandlerSocketNET.InitComponent;
 begin
   inherited;
@@ -142,12 +292,23 @@ begin
      if Assigned(FClientCertificates) then
      begin
        FSSL.AuthenticateAsClient(FHost,FClientCertificates,FEnabledProtocols,True);
+       if not FSSL.IsMutuallyAuthenticated then
+       begin
+         raise EIdSSLNotAuthenticated.Create('Not authenticated');
+       end;
      end
      else
      begin
        FSSL.AuthenticateAsClient(FHost,nil,FEnabledProtocols,True);
-     end;  
-
+       if not FSSL.IsAuthenticated then
+       begin
+         raise EIdSSLNotAuthenticated.Create('Not authenticated');
+       end;
+     end;
+  end;
+  if Assigned(FOnSSLHandshakeDone) then
+  begin
+    FOnSSLHandshakeDone(Self);
   end;
 end;
 
@@ -201,6 +362,7 @@ begin
       Result := True;
     end;
   end;
+  Result := True;
 end;
 
 end.
