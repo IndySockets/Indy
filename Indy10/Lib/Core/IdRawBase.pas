f3d7ada89b6d3158d1ef918d80e9d7a953adee8e
J. Peter Mugaas <indy@svn.indy.org>
2007-08-27 17:41:58 +0000

    Adjustment for changes in DEFINES.

--- Indy10/Lib/Core/IdRawBase.pas
+++ Indy10/Lib/Core/IdRawBase.pas
@@ -169,7 +169,7 @@ begin
       {$ELSE}
       FBinding.AllocateSocket(Id_SOCK_RAW, FProtocolIPv6);
       {$ENDIF}
-      {$IFNDEF DOTNET1}
+      {$IFNDEF DOTNET1_1}
       {
       Microsoft NET Framework 1.1 may actually have the packetinfo option but that
       will not do you any good because you need a RecvMsg function which is not
