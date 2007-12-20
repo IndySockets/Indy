c153657b9edfe39bce1dde53de7530e0525aa9b6
J. Peter Mugaas <indy@svn.indy.org>
2007-12-20 09:12:44 +0000

    Now works properly in Win32.  It still doesn't work right in DotNET.  It's probably a bug someplace in our NET code but I'm not sure where.

--- Indy10/Lib/Protocols/IdSASLDigest.pas
+++ Indy10/Lib/Protocols/IdSASLDigest.pas
@@ -9,12 +9,9 @@ type
   TIdSASLDigest = class(TIdSASLUserPass)
   protected
     Fauthzid : String;
-    FDebugCNonce : String;
   public
     function StartAuthenticate(const AChallenge, AHost, AProtocolName:string) : String; override;
     class function ServiceName: TIdSASLServiceName; override;
-    //temp property until I finish debugging this.
-    property DebugCNonce : string read FDebugCNonce write FDebugCNonce;
   published
     property authzid : String read Fauthzid write Fauthzid;
   end;
@@ -24,15 +21,17 @@ type
   EIdSASLDigestChallInvalidAlg  = class(EIdSASLDigestChallException);
   EIdSASLDigestAuthConfNotSupported = class(EIdSASLDigestException);
 
+//done this way so we can use testboxes
+
+function CalcDigestResponse(const AUserName, APassword, ARealm, ANonce, ACNonce : String;
+  const ANC : Integer;
+  const  AQop, ADigestURI : String; const AAuthzid : String = '') : String;
+
 implementation
 uses IdGlobal, IdHash, IdHashMessageDigest, IdResourceStringsProtocols, SysUtils;
 
 const
-  //NC should eventually be made into a var (probalby an integer
-  NC='00000001';  {do not localize}
   SASL_DIGEST_METHOD = 'AUTHENTICATE:';  {do not localize}
-  //specified by RFC 2831
-  A2_HASHSUM = '00000000000000000000000000000000';  {do not localize}
 
 function RemoveQuote(const aStr:string):string;
 {$IFDEF USEINLINE} inline; {$ENDIF}
@@ -44,15 +43,104 @@ begin
   end;
 end;
 
-function HashResultAsHex(const AStr : String) : String;
+function HashResult(const AStr : String): TIdBytes;
+{$IFDEF USEINLINE} inline; {$ENDIF}
+begin
+   with TIdHashMessageDigest5.Create do
+   try
+     Result := HashString(AStr);
+   finally
+     Free;
+   end;
+end;
+
+function HashResultAsHex(const ABytes : TIdBytes) : String; overload;
 {$IFDEF USEINLINE} inline; {$ENDIF}
 begin
   with TIdHashMessageDigest5.Create do
   try
+
+    Result := HashBytesAsHex(ABytes);
+  finally
+    Free;
+  end;
+end;
+
+function HashResultAsHex(const AStr : String) : String; overload;
+{$IFDEF USEINLINE} inline; {$ENDIF}
+begin
+  with TIdHashMessageDigest5.Create do
+  try
+
     Result := LowerCase(HashStringAsHex(AStr));
   finally
     Free;
   end;
+end;
+
+function NCToStr(const ANC : Integer): String;
+{$IFDEF USEINLINE} inline; {$ENDIF}
+begin
+  Result := IntToHex(ANC, 8);
+end;
+
+function CalcDigestResponse(const AUserName, APassword, ARealm, ANonce, ACNonce : String;
+  const ANC : Integer;
+  const  AQop, ADigestURI : String; const AAuthzid : String = '') : String;
+var
+  LA1, LA2, LH_A1, LH_A2, LNC : String;
+begin
+  LNC := NCToStr(ANC);
+
+//
+//   If authzid is specified, then A1 is
+//
+//
+//      A1 = { H( { username-value, ":", realm-value, ":", passwd } ),
+//           ":", nonce-value, ":", cnonce-value, ":", authzid-value }
+//
+//   If authzid is not specified, then A1 is
+//
+//
+//      A1 = { H( { username-value, ":", realm-value, ":", passwd } ),
+//           ":", nonce-value, ":", cnonce-value }
+//
+//   where
+//
+//         passwd   = *OCTET
+
+    LA1 := BytesToString(HashResult(AUserName + ':' + ARealm + ':' + APassword));
+    LA1 := LA1 + ':' + ANonce + ':'+ ACNonce;
+    if AAuthzid<>'' then begin
+      LA1 := LA1 + ':'+AAuthzid;
+    end;
+    LH_A1 := HashResultAsHex(LA1);
+//   If the "qop" directive's value is "auth", then A2 is:
+//
+//      A2       = { "AUTHENTICATE:", digest-uri-value }
+//
+//   If the "qop" value is "auth-int" or "auth-conf" then A2 is:
+//
+//      A2       = { "AUTHENTICATE:", digest-uri-value,
+//               ":00000000000000000000000000000000" }
+    LA2 := SASL_DIGEST_METHOD + ADigestURI;
+    if AQop = 'auth-int' then begin
+      LA2 := Result +':00000000000000000000000000000000';
+    end;
+    LH_A2 := HashResultAsHex(LA2);
+//RFC 2831
+//     response-value  =
+//
+//         HEX( KD ( HEX(H(A1)),
+//                 { nonce-value, ":" nc-value, ":",
+//                   cnonce-value, ":", qop-value, ":", HEX(H(A2)) }))
+//
+//    //result
+    Result := HashResultAsHex( LH_A1 + ':' + ANonce + ':'+LNC+':'+ACNonce+':'+AQop+':'+LH_A2);
+
+//    if LQopOptions.IndexOf('auth-conf') > -1 then begin
+//      EIdSASLDigestAuthConfNotSupported.IfFalse(LQopOptions.IndexOf('auth')>-1,RSSASLDigestAuthConfNotSupported);
+//    end;
 end;
 
 { TIdSASLDigest }
@@ -70,8 +158,6 @@ var LBuf : String;
   Lqop : String;
   LstrCNonce : String;
   i : Integer;
-  LA1, LA2 : String;
-  LH_A1, LH_A2 : String;
   LstrResponse : String;
   LURL : String;
   LCharset : String;
@@ -112,70 +198,20 @@ begin
     LRealm :=  LChallange.Values['realm'];
     LAlgorithm :=  LChallange.Values['algorithm'];
     EIdSASLDigestChallNoAlgorithm.IfFalse(LAlgorithm<>'',RSSASLDigestMissingAlgorithm);
-    EIdSASLDigestChallInvalidAlg.IfFalse(LAlgorithm = 'md5-sess',RSSASLDigestInvalidAlgorithm);
+//    EIdSASLDigestChallInvalidAlg.IfFalse(LAlgorithm = 'md5-sess',RSSASLDigestInvalidAlgorithm);
 
     //Commented out for case test mentioned in RFC 2831
-    if FDebugCNonce<>'' then
-    begin
-      LstrCNonce := FDebugCNonce;
-    end
-    else
-    begin
-      LstrCNonce := HashResultAsHex(DateTimeToStr(Now));
-    end;
-    LCharset :=  LChallange.Values['charset'];
-//
-//   If authzid is specified, then A1 is
-//
-//
-//      A1 = { H( { username-value, ":", realm-value, ":", passwd } ),
-//           ":", nonce-value, ":", cnonce-value, ":", authzid-value }
-//
-//   If authzid is not specified, then A1 is
-//
-//
-//      A1 = { H( { username-value, ":", realm-value, ":", passwd } ),
-//           ":", nonce-value, ":", cnonce-value }
-//
-//   where
-//
-//         passwd   = *OCTET
+    LstrCNonce := HashResultAsHex(DateTimeToStr(Now));
 
-    LA1 := GetUsername + ':' + LRealm + ':' + GetPassword;
-    if TextIsSame(LAlgorithm, 'MD5-sess') then begin
-      if Fauthzid='' then begin
-        LA1 := LA1 + ':' + LNonce + ':' + LstrCNonce;
-      end else begin
-        LA1 := LA1 + ':' + LNonce + ':' + LstrCNonce+':'+Fauthzid;
-      end;
-    end;
-    LH_A1 := HashResultAsHex(LA1);
-    if LQop = 'auth-int' then begin
-      LA2 := SASL_DIGEST_METHOD + ':' + LUrl + ':'+A2_HASHSUM;
-    end else begin
-      LA2 := SASL_DIGEST_METHOD + ':' + LUrl;
-    end;
-    LH_A2 := HashResultAsHex(LA2);
-//RFC 2831
-//     response-value  =
-//
-//         HEX( KD ( HEX(H(A1)),
-//                 { nonce-value, ":" nc-value, ":",
-//                   cnonce-value, ":", qop-value, ":", HEX(H(A2)) }))
-//
-//    //result
-    LstrResponse := HashResultAsHex( LH_A1 + ':' + LNonce + ':'+NC+':'+LstrCNonce+':'+LQop+':'+LH_A2);
+    LCharset :=  LChallange.Values['charset'];
+
+    LstrResponse := CalcDigestResponse(GetUserName,Self.GetPassword,LRealm,LNonce,LstrCNonce,
+     1, Lqop, LURL,Fauthzid);
+
 
 //    if LQopOptions.IndexOf('auth-conf') > -1 then begin
 //      EIdSASLDigestAuthConfNotSupported.IfFalse(LQopOptions.IndexOf('auth')>-1,RSSASLDigestAuthConfNotSupported);
 //    end;
-
-{
-#( username | realm | nonce | cnonce |
-    nonce-count | qop | digest-uri | response |
-    maxbuf | charset | cipher | authzid |
-    auth-param )
-}
 
    if LCharset='' then
    begin
@@ -185,10 +221,16 @@ begin
    begin
      Result := 'charset='+LCharset+',';
    end;
+{
+#( username | realm | nonce | cnonce |
+    nonce-count | qop | digest-uri | response |
+    maxbuf | charset | cipher | authzid |
+    auth-param )
+}
     Result := Result + 'username="'+GetUsername+'"'+
      ',realm="'+LRealm+'"'+ {Do not localize}
       ',nonce="'+ LNonce+'"'+
-      ',nc='+NC+
+      ',nc='+NCToStr(1)+
       ',cnonce="'+LstrCNonce+'"'+
       ',digest-uri="'+LURL+'"'+
       ',response='+LstrResponse+
