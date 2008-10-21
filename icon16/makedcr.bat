060b31649f8c45657cf7b5cc9c9aba5e73dc21b5
JPeterMugaas <indy@svn.indy.org>
2008-10-21 17:14:24 +0000

    Updates for Tiburon branch.

--- icon16/makedcr.bat
+++ icon16/makedcr.bat
@@ -1,6 +1,8 @@
 brc32 IdRegister.rc -r -foIdRegister.dcr
 copy IdRegister.dcr ..\Lib\Protocols
+copy IdRegister.dcr ..\..\branches\Tiburon\Lib\Protocols
 brc32 IdCoreRegister.rc -r -foIdCoreRegister.dcr
 copy IdCoreRegister.dcr ..\Lib\Core
+copy IdRegister.dcr ..\..\branches\Tiburon\Lib\Protocols
 brc32 IdSuperCoreRegister.rc -r -foIdSuperCoreRegister.dcr
 copy IdSuperCoreRegister.dcr ..\Lib\SuperCore
--- icons/makedcr.bat
+++ icons/makedcr.bat
@@ -1,6 +1,8 @@
 brc32 IdRegister.rc -rv -foIdRegisterCool.dcr
 copy IdRegisterCool.dcr ..\lib\Protocols
+copy IdRegisterCool.dcr ..\..\branches\Tiburon\Lib\Protocols
 brc32 IdCoreRegister.rc -rv -foIdCoreRegisterCool.dcr
 copy IdCoreRegisterCool.dcr ..\lib\Core
+copy IdCoreRegisterCool.dcr ..\..\branches\Tiburon\Lib\Core
 brc32 IdSuperCoreRegister.rc -rv -foIdSuperCoreRegisterCool.dcr
 copy IdSuperCoreRegisterCool.dcr ..\lib\SuperCore
