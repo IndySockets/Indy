05afedce5345780508c7e40d20931fdbe104ae15
JPeterMugaas <indy@svn.indy.org>
2008-10-21 17:15:42 +0000

    Expansions for Tiburon branch.

--- Builder/VersionMarker/VersionMarker.dpr
+++ Builder/VersionMarker/VersionMarker.dpr
@@ -29,7 +29,6 @@ program VersionMarker;
 {$APPTYPE CONSOLE}
 
 uses
-  ExceptionLog,
   Classes,
   INIFiles,
   SysUtils;
@@ -47,8 +46,12 @@ const
 //for them
 
 const
-  xIdVersFile = 'W:\source\Indy10\Lib\System\IdVers.inc';
+ // xIdVersFile = 'W:\source\Indy10\Lib\System\IdVers.inc';
+  gIdVersFile = 'Lib\System\IdVers.inc';
+
 var
+  gBasePath : string;
+  gIniPath : String;
   xINI: TINIFile;
   xVerMajor: Integer;
   xVerMinor: Integer;
@@ -83,25 +86,42 @@ begin
   end;
 end;
 
+function CurrentYear : Word;
+var lm, ld : Word;
+begin
+  SysUtils.DecodeDate(Now,Result,lm,ld);
+end;
+
 procedure Main;
 
 begin
+  //find base path
+  if ParamCount > 0 then
+  begin
+    gBasePath := ParamStr(1)+'\';
+  end
+  else
+  begin
+    gBasePath := ExtractFilePath(ParamStr(0));
+  end;
+  gIniPath := gBasePath+'Builder\VersionMarker\'+ExtractFileName(ChangeFileExt(ParamStr(0), '.ini'));
   try
-    xINI := TINIFile.Create(ChangeFileExt(ParamStr(0), '.ini')); try
+    //10.2.5
+    xINI := TINIFile.Create(gIniPath); try
       xVerMajor := xINI.ReadInteger('Version', 'Major', 10);
       xVerMinor := xINI.ReadInteger('Version', 'Minor', 0);
       xVerPoint := xINI.ReadInteger('Version', 'Point', 60) + 1;
       xVerString := IntToStr(xVerMajor) + '.' + IntToStr(xVerMinor) + '.'
         + IntToStr(xVerPoint);
-      xCopyright := xINI.ReadString('Version','Copyright','Copyright © 1993 - 2005 Chad Z. Hower a.k.a Kudzu and the Indy Pit Crew');
+      xCopyright := xINI.ReadString('Version','Copyright','Copyright © 1993 - '+ IntToStr(CurrentYear)+ ' Chad Z. Hower a.k.a Kudzu and the Indy Pit Crew');
       xCompany := xINI.ReadString('Version','Company','Chad Z. Hower a.k.a Kudzu and the Indy Pit Crew');
       xTitle := xINI.ReadString('Version','Title','Indy');
       xDescription := xINI.ReadString('Version','Description','Internet Direct (Indy)');
 
       with TStringList.Create do try
-        LoadFromFile(xIdVersFile);
+        LoadFromFile(gBasePath + gIdVersFile);
         Strings[0] := '  gsIdVersion = ''' + xVerString + '''; {do not localize}';
-        SaveToFile(xIdVersFile);
+        SaveToFile(gBasePath + gIdVersFile);
       finally Free; end;
 
       // FB script for VS.NET
@@ -116,46 +136,52 @@ begin
       WriteLn(xVerString);
     finally FreeAndNil(xINI); end;
     //protocols packages - design time
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndy100.rc','w:\source\Indy10\Lib\Protocols\dclIndyProtocols100.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndy90.rc','w:\source\Indy10\Lib\Protocols\dclIndyProtocols90.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndy70.rc','w:\source\Indy10\Lib\Protocols\dclIndyProtocols70.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndy60.rc','w:\source\Indy10\Lib\Protocols\dclIndyProtocols60.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndy50.rc','w:\source\Indy10\Lib\Protocols\dclIndyProtocols50.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndy100.rc',gBasePath+'\Lib\Protocols\dclIndyProtocols120.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndy100.rc',gBasePath+'\Lib\Protocols\dclIndyProtocols100.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndy90.rc',gBasePath+'Lib\Protocols\dclIndyProtocols90.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndy70.rc',gBasePath+'Lib\Protocols\dclIndyProtocols70.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndy60.rc',gBasePath+'Lib\Protocols\dclIndyProtocols60.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndy50.rc',gBasePath+'Lib\Protocols\dclIndyProtocols50.rc');
     //protocols packages - run time
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\Indy100.rc','w:\source\Indy10\Lib\Protocols\IndyProtocols100.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\Indy90.rc','w:\source\Indy10\Lib\Protocols\IndyProtocols90.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\Indy70.rc','w:\source\Indy10\Lib\Protocols\IndyProtocols70.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\Indy60.rc','w:\source\Indy10\Lib\Protocols\IndyProtocols60.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\Indy50.rc','w:\source\Indy10\Lib\Protocols\IndyProtocols50.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\Indy100.rc',gBasePath+'Lib\Protocols\IndyProtocols120.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\Indy100.rc',gBasePath+'Lib\Protocols\IndyProtocols100.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\Indy90.rc',gBasePath+'Lib\Protocols\IndyProtocols90.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\Indy70.rc',gBasePath+'Lib\Protocols\IndyProtocols70.rc');
+
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\Indy60.rc',gBasePath+'Lib\Protocols\IndyProtocols60.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\Indy50.rc',gBasePath+'Lib\Protocols\IndyProtocols50.rc');
 
     //core package - design time
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndyCore100.rc','w:\source\Indy10\Lib\Core\dclIndyCore100.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndyCore90.rc','w:\source\Indy10\Lib\Core\dclIndyCore90.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndyCore70.rc','w:\source\Indy10\Lib\Core\dclIndyCore70.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndyCore60.rc','w:\source\Indy10\Lib\Core\dclIndyCore60.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\dclIndyCore50.rc','w:\source\Indy10\Lib\Core\dclIndyCore50.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndyCore120.rc',gBasePath+'Lib\Core\dclIndyCore120.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndyCore100.rc',gBasePath+'Lib\Core\dclIndyCore100.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndyCore90.rc',gBasePath+'Lib\Core\dclIndyCore90.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndyCore70.rc',gBasePath+'Lib\Core\dclIndyCore70.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndyCore60.rc',gBasePath+'Lib\Core\dclIndyCore60.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\dclIndyCore50.rc',gBasePath+'Lib\Core\dclIndyCore50.rc');
     //core package - run time
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndyCore100.rc','w:\source\Indy10\Lib\Core\IndyCore100.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndyCore90.rc','w:\source\Indy10\Lib\Core\IndyCore90.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndyCore70.rc','w:\source\Indy10\Lib\Core\IndyCore70.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndyCore60.rc','w:\source\Indy10\Lib\Core\IndyCore60.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndyCore50.rc','w:\source\Indy10\Lib\Core\IndyCore50.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndyCore120.rc',gBasePath+'Lib\Core\IndyCore120.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndyCore100.rc',gBasePath+'Lib\Core\IndyCore100.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndyCore90.rc',gBasePath+'Lib\Core\IndyCore90.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndyCore70.rc',gBasePath+'Lib\Core\IndyCore70.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndyCore60.rc',gBasePath+'Lib\Core\IndyCore60.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndyCore50.rc',gBasePath+'Lib\Core\IndyCore50.rc');
     //core package - run time
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndySystem100.rc','w:\source\Indy10\Lib\System\IndySystem100.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndySystem90.rc','w:\source\Indy10\Lib\System\IndySystem90.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndySystem70.rc','w:\source\Indy10\Lib\System\IndySystem70.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndySystem60.rc','w:\source\Indy10\Lib\System\IndySystem60.rc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IndySystem50.rc','w:\source\Indy10\Lib\System\IndySystem50.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndySystem120.rc',gBasePath+'Lib\System\IndySystem120.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndySystem100.rc',gBasePath+'Lib\System\IndySystem100.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndySystem90.rc',gBasePath+'Lib\System\IndySystem90.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndySystem70.rc',gBasePath+'Lib\System\IndySystem70.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndySystem60.rc',gBasePath+'Lib\System\IndySystem60.rc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IndySystem50.rc',gBasePath+'Lib\System\IndySystem50.rc');
     // Borland Delphi Studio Packages for DotNET version info
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IdSystem90ASM90.inc','w:\source\Indy10\Lib\System\IdSystem90ASM90.inc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IddclCore90ASM90.inc','w:\source\Indy10\Lib\Core\IddclCore90ASM90.inc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IdCore90ASM90.inc','w:\source\Indy10\Lib\Core\IdCore90ASM90.inc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IddclProtocols90ASM90.inc','w:\source\Indy10\Lib\Protocols\IddclProtocols90ASM90.inc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IdProtocols90ASM90.inc','w:\source\Indy10\Lib\Protocols\IdProtocols90ASM90.inc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IddclSecurity90ASM90.inc','w:\source\Indy10\Lib\Security\IddclSecurity90ASM90.inc');
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IdSecurity90ASM90.inc','w:\source\Indy10\Lib\Security\IdSecurity90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IdSystem90ASM90.inc',gBasePath+'Lib\System\IdSystem90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IddclCore90ASM90.inc',gBasePath+'Lib\Core\IddclCore90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IdCore90ASM90.inc',gBasePath+'Lib\Core\IdCore90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IddclProtocols90ASM90.inc',gBasePath+'Lib\Protocols\IddclProtocols90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IdProtocols90ASM90.inc',gBasePath+'Lib\Protocols\IdProtocols90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IddclSecurity90ASM90.inc',gBasePath+'Lib\Security\IddclSecurity90ASM90.inc');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IdSecurity90ASM90.inc',gBasePath+'Lib\Security\IdSecurity90ASM90.inc');
     //IdAssemblyInfo for Visual Studio .NET
-    ReplaceInFile(ExtractFilePath(ParamStr(0))+ 'Data\IdAssemblyInfo.pas','w:\source\Indy10\Lib\System\IdAssemblyInfo.pas');
+    ReplaceInFile(gBasePath+ 'Builder\VersionMarker\Data\IdAssemblyInfo.pas',gBasePath+'Lib\System\IdAssemblyInfo.pas');
   except
     on E: Exception do begin
       WriteLn(E.Message);
