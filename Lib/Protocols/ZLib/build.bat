642af1e8a25082edc7c75a203c292cb987f29b51
JPeterMugaas <indy@svn.indy.org>
2010-09-11 14:47:21 +0000

    Zlib 1.2.5 built with Borland C++ 5.5.1.  Refinement for build process to ensure that the proper bcc32.exe is used.

--- Lib/Protocols/ZLib/build.bat
+++ Lib/Protocols/ZLib/build.bat
@@ -1,2 +1,9 @@
-c:\Progra~1\borland\BDS\3.0\bin\make -f zlibd32.mak
+echo off
+set OLDPATH=%PATH%
+rem path c:\Progra~1\Borland\BDS\3.0\bin
+path C:\Borland\BCC55\bin
+echo %PATH%
+rem c:\Progra~1\Borland\BDS\3.0\bin
+make -f zlibd32.mak
+path %OLDPATH%
 copy *.obj ..
