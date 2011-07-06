e6dcda0f0ec036d76cfdedd105e8b03c7cd8052f
RemyLebeau <Indy-@svn.indy.org>
2011-07-06 06:01:06 +0000

    Changed Fulld11.bat to reference the 110* packages instead of the 120* packages.

--- Lib/Fulld11.bat
+++ Lib/Fulld11.bat
@@ -28,27 +28,27 @@ ECHO ****************
 ECHO  Compile System
 ECHO ****************
 CD System
-%NDD11%\Bin\dcc32.exe IndySystem120.dpk /Oobjs /DBCB /m /h /w /JPHNE /N..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
-copy *120.bpl ..\..\D11 >nul
-copy *120.dcp ..\..\D11 >nul
+%NDD11%\Bin\dcc32.exe IndySystem110.dpk /Oobjs /DBCB /m /h /w /JPHNE /N..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
+copy *110.bpl ..\..\D11 >nul
+copy *110.dcp ..\..\D11 >nul
 copy *.hpp ..\..\D11 >nul
 if errorlevel 1 goto enderror
-copy ..\..\D11\IndySystem120.bpl %NDWINSYS% >nul
+copy ..\..\D11\IndySystem110.bpl %NDWINSYS% >nul
 CD ..
 
 ECHO **************
 ECHO  Compile Core    
 ECHO **************
 CD Core
-%NDD11%\Bin\dcc32.exe IndyCore120.dpk /Oobjs /DBCB /m /h /w /JPHNE /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
+%NDD11%\Bin\dcc32.exe IndyCore110.dpk /Oobjs /DBCB /m /h /w /JPHNE /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
 if errorlevel 1 goto enderror
-%NDD11%\Bin\dcc32.exe dclIndyCore120.dpk /Oobjs /DBCB /m /h /w /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
+%NDD11%\Bin\dcc32.exe dclIndyCore110.dpk /Oobjs /DBCB /m /h /w /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
 if errorlevel 1 goto enderror
-copy *120.bpl ..\..\D11 >nul
-copy *120.dcp ..\..\D11 >nul
+copy *110.bpl ..\..\D11 >nul
+copy *110.dcp ..\..\D11 >nul
 copy *.hpp ..\..\D11 >nul
-copy ..\..\D11\IndyCore120.bpl %NDWINSYS% >nul
-copy ..\..\D11\dclIndyCore120.bpl %NDWINSYS% >nul
+copy ..\..\D11\IndyCore110.bpl %NDWINSYS% >nul
+copy ..\..\D11\dclIndyCore110.bpl %NDWINSYS% >nul
 CD ..
 
 ECHO *******************
@@ -62,16 +62,16 @@ ECHO ************************
 %NDD11%\Bin\dcc32.exe -DBCB -B -M -JPHNE -N..\..\D11 /U..\..\D11 -H -W -Z IdCompressionIntercept.pas -$d-l-
 if errorlevel 1 goto enderror
 
-%NDD11%\Bin\dcc32.exe IndyProtocols120.dpk /Oobjs /DBCB /m /h /w /JPHNE /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
+%NDD11%\Bin\dcc32.exe IndyProtocols110.dpk /Oobjs /DBCB /m /h /w /JPHNE /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
 if errorlevel 1 goto enderror
-%NDD11%\Bin\dcc32.exe dclIndyProtocols120.dpk /Oobjs /DBCB /m /h /w /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
+%NDD11%\Bin\dcc32.exe dclIndyProtocols110.dpk /Oobjs /DBCB /m /h /w /N..\..\D11 /U..\..\D11 -$d-l-n+p+r-s-t-w- %2 %3 %4
 if errorlevel 1 goto enderror
 
-copy *120.bpl ..\..\D11 >nul
-copy *120.dcp ..\..\D11 >nul
+copy *110.bpl ..\..\D11 >nul
+copy *110.dcp ..\..\D11 >nul
 copy *.hpp ..\..\D11 >nul
-copy ..\..\D11\IndyProtocols120.bpl %NDWINSYS% >nul
-copy ..\..\D11\dclIndyProtocols120.bpl %NDWINSYS% >nul
+copy ..\..\D11\IndyProtocols110.bpl %NDWINSYS% >nul
+copy ..\..\D11\dclIndyProtocols110.bpl %NDWINSYS% >nul
 CD ..
 
 goto endok
