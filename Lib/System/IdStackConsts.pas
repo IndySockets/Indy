d2600ccf3b706d1171d866f12293efe53fa393ea
RemyLebeau <Indy-@svn.indy.org>
2013-01-02 00:02:39 +0000

    Workaround for missing SO_DONTLINGER constant on FPC for Linux.

--- Lib/System/IdStackConsts.pas
+++ Lib/System/IdStackConsts.pas
@@ -378,6 +378,12 @@ const
 
   // Socket options
   {$IFNDEF DOTNET}
+    {$IFNDEF WINDOWS}
+      {$IFDEF FPC}
+  SO_DONTLINGER          =  not SO_LINGER;
+  {$EXTERNALSYM SO_DONTLINGER}
+      {$ENDIF}
+    {$ENDIF}
   Id_SO_BROADCAST        =  SO_BROADCAST;
   Id_SO_DEBUG            =  SO_DEBUG;
   Id_SO_DONTLINGER       =  SO_DONTLINGER;
@@ -397,11 +403,12 @@ SocketOptionName.AddSourceMembership;//  Join a source group.
 SocketOptionName.BlockSource;//  Block data from a source.
 }
   Id_SO_BROADCAST        =  SocketOptionName.Broadcast;//  Permit sending broadcast messages on the socket.
-  Id_SO_DEBUG            =  SocketOptionName.Debug;
 {
 SocketOptionName.BsdUrgent;//  Use urgent data as defined in RFC-1222. This option can be set only once, and once set, cannot be turned off.
 SocketOptionName.ChecksumCoverage;//  Set or get UDP checksum coverage.
-SocketOptionName.Debug;//  Record debugging information.
+}
+  Id_SO_DEBUG            =  SocketOptionName.Debug;//  Record debugging information.
+{
 SocketOptionName.DontFragment;//  Do not fragment IP datagrams.
 }
   Id_SO_DONTLINGER       =  SocketOptionName.DontLinger;//  Close socket gracefully without lingering.
