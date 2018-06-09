692da7c00e43be4a3847e7ad6b59b976ef38ccd5
RemyLebeau <Indy-@svn.indy.org>
2018-06-09 00:09:10 +0000

    Updating a URL in a couple of comments

--- Lib/System/IdStackVCLPosix.pas
+++ Lib/System/IdStackVCLPosix.pas
@@ -498,7 +498,7 @@ function getifaddrs(var ifap: pifaddrs): Integer; cdecl; external libc name _PU 
 procedure freeifaddrs(ifap: pifaddrs); cdecl; external libc name _PU + 'freeifaddrs'; {do not localize}
 {$ELSE}
   {$IFDEF ANDROID}
-  // TODO: implement getifaddrs() manually using code from https://github.com/kmackay/android-ifaddrs
+  // TODO: implement getifaddrs() manually using code from https://github.com/morristech/android-ifaddrs
   {.$DEFINE HAS_getifaddrs}
   {$ENDIF}
 {$ENDIF}
@@ -561,7 +561,7 @@ begin
 
   {$ELSE}
 
-  // TODO: on Android, either implement getifaddrs() (https://github.com/kmackay/android-ifaddrs)
+  // TODO: on Android, either implement getifaddrs() (https://github.com/morristech/android-ifaddrs)
   // or use the Java API to enumerate the local network interfaces and their IP addresses, eg:
   {
   var
