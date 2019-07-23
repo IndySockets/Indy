9033f20ef4cefef133af6984d95fc38a0af9f6f3
RemyLebeau <Indy-@svn.indy.org>
2019-07-23 22:34:27 +0000

    Making TIdMessage GenerateHeader() and ProcessHeaders() methods virtual, per request from Embarcadero.

--- Lib/Protocols/IdMessage.pas
+++ Lib/Protocols/IdMessage.pas
@@ -439,7 +439,7 @@ type
     procedure Clear; virtual;
     procedure ClearBody;
     procedure ClearHeader;
-    procedure GenerateHeader;
+    procedure GenerateHeader; virtual;
     procedure InitializeISO(var VHeaderEncoding: Char; var VCharSet: String);
     function  IsBodyEncodingRequired: Boolean;
     function  IsBodyEmpty: Boolean;
@@ -447,7 +447,7 @@ type
     procedure LoadFromFile(const AFileName: string; const AHeadersOnly: Boolean = False);
     procedure LoadFromStream(AStream: TStream; const AHeadersOnly: Boolean = False);
 
-    procedure ProcessHeaders;
+    procedure ProcessHeaders; virtual;
 
     procedure SaveToFile(const AFileName : string; const AHeadersOnly: Boolean = False);
     procedure SaveToStream(AStream: TStream; const AHeadersOnly: Boolean = False);
