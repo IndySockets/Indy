b9f5d376a1eb06686e02712da3af0e51d1136c56
RemyLebeau <Indy-@svn.indy.org>
2019-07-24 19:26:10 +0000

    More Embarcadero patches

--- Lib/Protocols/IdMessageClient.pas
+++ Lib/Protocols/IdMessageClient.pas
@@ -936,7 +936,7 @@ begin
       if IsHeaderMediaType(AMsg.ContentType, 'application/mac-binhex40') then begin  {Do not Localize}
         LContentTransferEncoding := 'binhex40'; {do not localize}
       end
-      else if (AMsg.Encoding = meMIME) and (AMsg.MIMEBoundary.Count > 0) then begin
+      else if (AMsg.Encoding = meMIME) and (AMsg.MIMEBoundary.Count > 0) and (not AMsg.NoDecode) then begin
         LContentTransferEncoding := '7bit'; {do not localize}
       end;
     end
@@ -1556,6 +1556,7 @@ begin
   LIOHandler := TIdIOHandlerStreamMsg.Create(nil, AStream);
   try
     LIOHandler.FreeStreams := False;
+    LIOHandler.MaxLineLength := MaxInt;
     IOHandler := LIOHandler;
     try
       IOHandler.Open;
