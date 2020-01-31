08a0075fd6541a40849817dbf29fb9fc2c7a032c
Bi0T1N <Bi0T1N@users.noreply.github.com>
2020-01-31 21:00:25 +0000

    Define global SSLVersion in IdSSL
    this allows to have one set of SSL versions
    for all SSL IO handler

--- Lib/Protocols/IdSSLOpenSSL.pas
+++ Lib/Protocols/IdSSLOpenSSL.pas
@@ -234,8 +234,6 @@ uses
   IdYarn;
 
 type
-  TIdSSLVersion = (sslvSSLv2, sslvSSLv23, sslvSSLv3, sslvTLSv1,sslvTLSv1_1,sslvTLSv1_2);
-  TIdSSLVersions = set of TIdSSLVersion;
   TIdSSLMode = (sslmUnassigned, sslmClient, sslmServer, sslmBoth);
   TIdSSLVerifyMode = (sslvrfPeer, sslvrfFailIfNoPeerCert, sslvrfClientOnce);
   TIdSSLVerifyModeSet = set of TIdSSLVerifyMode;
